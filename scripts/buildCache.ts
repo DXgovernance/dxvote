d4fd0c32146887eb1d83492f259f3026a437160c
AugustoL <me@augustol.com>
2021-04-20 19:56:04 -0300

    feat(scripts): Finish logic in the script to generate cache file

--- scripts/buildCache.ts
+++ scripts/buildCache.ts
@@ -3,7 +3,7 @@ const { BigNumber } = require('bignumber.js');
 const web3 = hre.web3;
 const rinkebyContracts = require('../src/config/rinkeby');
 const { decodePermission } = require('./helpers/permissions');
-const { getEventsBetweenBlocks, sortEvents, decodeSchemeParameters } = require('./helpers/cache');
+const { getEventsBetweenBlocks, sortEvents, decodeSchemeParameters, decodeStatus } = require('./helpers/cache');
 
 // The only way I found to import the types from the source folder into here was by copy/pasting
 // TO DO: Use the interfaces imported the source folder
@@ -53,6 +53,7 @@ enum VotingMachineProposalState {
 interface ProposalInfo {
   id: string;
   scheme: string;
+  title: string;
   to: string[];
   callData: string[];
   values: typeof BigNumber[];
@@ -75,7 +76,7 @@ interface ProposalInfo {
   preBoostedPhaseTime: typeof BigNumber;
   daoRedeemItsWinnings: boolean;
   status: string;
-  statusPriority: number;
+  priority: number;
   boostTime: number;
   finishTime: number;
   shouldBoost: boolean,
@@ -85,8 +86,6 @@ interface ProposalInfo {
   preBoostedNegativeVotes: typeof BigNumber;
   positiveStakes: typeof BigNumber;
   negativeStakes: typeof BigNumber;
-  tokenRewards: {[address: string]: boolean};
-  repRewards: {[address: string]: boolean};
 }
 
 interface SchemeParameters {
@@ -123,37 +122,36 @@ interface SchemeInfo {
   registered: boolean;
   address: string;
   name: string,
-  parametersHash: string;
+  paramsHash: string;
   controllerAddress: string;
   ethBalance: typeof BigNumber;
   parameters: SchemeParameters;
   permissions: SchemePermissions;
-  proposals: ProposalInfo[];
   proposalIds: string[];
-  boostedProposals: number;
   callPermissions: SchemeCallPermission[];
 }
 
 interface DaoInfo {
   address: string;
   totalRep: typeof BigNumber;
+  repHolders: {[id: string]: typeof BigNumber};
   ethBalance: typeof BigNumber;
-  userEthBalance: typeof BigNumber;
-  userRep: typeof BigNumber;
-  userVotingMachineTokenBalance: typeof BigNumber;
-  userVotingMachineTokenApproved: typeof BigNumber;
+  dxdBalance: typeof BigNumber;
+  boostedProposals: number;
 }
 
 interface DaoCache {
+  blockNumber: number;
   daoInfo: DaoInfo
   schemes: {[address: string]: SchemeInfo};
   proposals: {[id: string]: ProposalInfo};
-  blockNumber: number;
-  votes: Vote[];
-  stakes: Stake[];
-  redeems: Redeem[];
-  redeemsRep: RedeemRep[];
-  proposalStateChanges: ProposalStateChange[];
+  votingMachineEvents: {
+    votes: Vote[];
+    stakes: Stake[];
+    redeems: Redeem[];
+    redeemsRep: RedeemRep[];
+    proposalStateChanges: ProposalStateChange[];
+  }
 }
 
 // Require the artifacts contracts
@@ -161,40 +159,54 @@ const WalletScheme = hre.artifacts.require("WalletScheme");
 const PermissionRegistry = hre.artifacts.require("PermissionRegistry");
 const DxController = hre.artifacts.require("DxController");
 const DxAvatar = hre.artifacts.require("DxAvatar");
+const DxReputation = hre.artifacts.require("DxReputation");
 const DXDVotingMachine = hre.artifacts.require("DXDVotingMachine");
+const ERC20 = hre.artifacts.require("ERC20");
 
 async function main() {
   // Set block range for the script to run
   const fromBlock = rinkebyContracts.fromBlock;
   const toBlock = await web3.eth.getBlockNumber();
   
+  console.log('Runnig build cche script from block', fromBlock, ' to block', toBlock);
+  
+  // Instantiate the contracts
+  const votingMachine = await DXDVotingMachine.at(rinkebyContracts.votingMachine);
+  const avatar = await DxAvatar.at(rinkebyContracts.avatar);
+  const controller = await DxController.at(rinkebyContracts.controller);
+  const reputation = await DxReputation.at(rinkebyContracts.reputation);
+  const permissionRegistry = await PermissionRegistry.at(rinkebyContracts.permissionRegistry);
+  const dxd = await ERC20.at(rinkebyContracts.votingMachineToken);
+  
   let cache: DaoCache = {
+    blockNumber: toBlock,
     daoInfo: {} as DaoInfo,
     schemes: {},
     proposals: {},
-    blockNumber: toBlock,
-    votes: [],
-    stakes: [],
-    redeems: [],
-    redeemsRep: [],
-    proposalStateChanges: [],
+    votingMachineEvents: {
+      votes: [],
+      stakes: [],
+      redeems: [],
+      redeemsRep: [],
+      proposalStateChanges: []
+    }
   };
-
-  // Instantiate the contracts
-  const votingMachine = await DXDVotingMachine.at(rinkebyContracts.votingMachine);
-  const masterWalletScheme = await WalletScheme.at(rinkebyContracts.masterWalletScheme);
-  const quickWalletScheme = await WalletScheme.at(rinkebyContracts.quickWalletScheme);
-  const avatar = await DxAvatar.at(rinkebyContracts.avatar);
-  const controller = await DxController.at(rinkebyContracts.controller);
-  const permissionRegistry = await PermissionRegistry.at(rinkebyContracts.permissionRegistry);
+  
+  // Update DaoInfo
+  cache.daoInfo.address = rinkebyContracts.avatar;
+  cache.daoInfo.totalRep = await reputation.totalSupply.call();
+  cache.daoInfo.ethBalance = new BigNumber(await web3.eth.getBalance(rinkebyContracts.avatar));
+  cache.daoInfo.dxdBalance = await dxd.balanceOf.call(rinkebyContracts.avatar);
+  cache.daoInfo.boostedProposals = await votingMachine.orgBoostedProposalsCnt.call(rinkebyContracts.avatar);
+  cache.daoInfo.repHolders = !cache.daoInfo.repHolders ? {} : cache.daoInfo.repHolders;
 
   // Get events from contracts
   let votingMachineEvents = sortEvents( await getEventsBetweenBlocks(votingMachine, fromBlock, toBlock));
-  let masterWalletSchemeEvents = sortEvents( await getEventsBetweenBlocks(masterWalletScheme, fromBlock, toBlock));
-  let quickWalletSchemeEvents = sortEvents( await getEventsBetweenBlocks(quickWalletScheme, fromBlock, toBlock));
   let controllerEvents = sortEvents( await getEventsBetweenBlocks(controller, fromBlock, toBlock));
   let permissionRegistryEvents = sortEvents( await getEventsBetweenBlocks(permissionRegistry, fromBlock, toBlock));
+  let reputationEvents = sortEvents( await getEventsBetweenBlocks(reputation, fromBlock, toBlock));
 
+  // Get all call permissions (up to date) to later be added in schemes
   let allCallPermissions: {[from: string]: SchemeCallPermission[]} = {};
 
   await Promise.all(permissionRegistryEvents.map(async (permissionRegistryEvent) => {
@@ -215,68 +227,141 @@ async function main() {
       const permissionIndex = allCallPermissions[eventValues.from].findIndex(i => i.asset === eventValues.asset && i.to === eventValues.to);
       allCallPermissions[eventValues.from].splice(permissionIndex, 1);
     }
+    
   }));
-  
-  // Get schemes registered and their information in registerScheme events in controller
-  let allSchemes: SchemeInfo[] = [];
+    
+  // Get all schemes and their information in registerScheme events in controller
   await Promise.all(controllerEvents.map(async (controllerEvent) => {
+    
+    // Add or update the scheme information, register scheme is used to add and updates scheme parametersHash
     if (controllerEvent.event == "RegisterScheme") {
       const schemeContract = await WalletScheme.at(controllerEvent.returnValues._scheme);
       const paramsHash = await schemeContract.voteParams.call();
-      const controllerAddress = await await schemeContract.controllerAddress.call();
+      const controllerAddress = await schemeContract.controllerAddress.call();
       
       // If controller address is set the permissions are taken using avatar address as sender
-      let callPermissions: SchemeCallPermission[] = (controllerAddress == avatar.address)
+      let callPermissions: SchemeCallPermission[] = (controllerAddress == controller.address)
         ? allCallPermissions[avatar.address] : allCallPermissions[schemeContract.address];
       
-      allSchemes.push({
+      cache.schemes[schemeContract.address] = {
+        registered: true,
         address: schemeContract.address,
         name: schemeContract.address == rinkebyContracts.masterWalletScheme ? "MasterWalletScheme" : "QuickWalletScheme",
-        parametersHash: paramsHash,
+        paramsHash: paramsHash,
         controllerAddress: controllerAddress,
         ethBalance: await web3.eth.getBalance(schemeContract.address),
+        
+        // Get and decode the full parameters from the voting machine using teh parametersHash
         parameters: decodeSchemeParameters(
           await await votingMachine.parameters.call(paramsHash)
         ),
+        
+        // Get and decode the permissions
         permissions: decodePermission(
           await controller.getSchemePermissions.call(controllerEvent.returnValues._scheme, avatar.address)
         ),
-        registered: true,
-        proposals: [],
+        
         proposalIds: [],
-        boostedProposals: 0,
         callPermissions: callPermissions
-      })
+      };
+    
+    // Mark scheme as not registered but save all previous data
     } else if (
       controllerEvent.event == "UnregisterScheme" && 
+      // This condition is added to skip the first scheme added (that is the dao creator account)
       (controllerEvent.returnValues._sender != controllerEvent.returnValues._scheme)
     ) {
-      allSchemes[allSchemes.findIndex(i => i.address === controllerEvent.returnValues._scheme)].registered = false;
+      cache.schemes[controllerEvent.returnValues._scheme].registered = false;
     }
-  }));
-  console.log(allSchemes);
-  
-  for (let i = 0; i < allSchemes.length; i++) {
-    cache.schemes[allSchemes[i].address] = allSchemes[i];
-  }
-  // Get all proposals and their latest state
-  await Promise.all(masterWalletSchemeEvents.map(async (schemeEvent) => {
-    if (schemeEvent.event == "NewCallProposal") {
-      console.log(schemeEvent.event)
-    }
+    
   }));
   
-  // Get all voting machine events
-  votingMachineEvents.map((votingMachineEvent) => {
-    console.log(votingMachineEvent.event)
+  
+  // Get all proposals information
+  await Promise.all(Object.keys(cache.schemes).map(async (schemeAddress) => {
+
+    const schemeContract = await WalletScheme.at(schemeAddress);
+
+    const schemeEvents = sortEvents( await getEventsBetweenBlocks(schemeContract, fromBlock, toBlock))
+    await Promise.all(schemeEvents.map(async (schemeEvent) => {
+
+      if (schemeEvent.event == "NewCallProposal") {
+        
+        // Get all the proposal information from the scheme and voting machine
+        const proposalId = schemeEvent.returnValues._proposalId;
+        const schemeProposalInfo = await schemeContract.getOrganizationProposal.call(proposalId);
+        const votingMachineProposalInfo = await votingMachine.proposals.call(proposalId);
+        const proposalStatusWithVotes = await votingMachine.proposalStatusWithVotes.call(proposalId);
+        const proposalTimes = await votingMachine.getProposalTimes.call(proposalId);
+        const proposalShouldBoost = await votingMachine.shouldBoost.call(proposalId);
+        
+        // Decode teh status texy and pririty that will be given in the dapp
+        const { status, priority, boostTime, finishTime } = decodeStatus(
+          votingMachineProposalInfo.state.toString(),
+          schemeProposalInfo.state.toString(),
+          new BigNumber(proposalTimes[0].toString()),
+          new BigNumber(proposalTimes[1].toString()),
+          new BigNumber(proposalTimes[2].toString()),
+          cache.schemes[schemeContract.address].parameters.queuedVotePeriodLimit,
+          cache.schemes[schemeContract.address].parameters.boostedVotePeriodLimit,
+          cache.schemes[schemeContract.address].parameters.quietEndingPeriod,
+          cache.schemes[schemeContract.address].parameters.preBoostedVotePeriodLimit,
+          proposalShouldBoost
+        );
+        
+        cache.proposals[proposalId] = {
+          id: proposalId,
+          scheme: schemeContract.address,
+          to: schemeProposalInfo.to,
+          title: schemeProposalInfo.title,
+          callData: schemeProposalInfo.callData,
+          values: schemeProposalInfo.value,
+          stateInScheme: schemeProposalInfo.state,
+          stateInVotingMachine: votingMachineProposalInfo.state,
+          descriptionHash: schemeProposalInfo.descriptionHash,
+          creationBlock: schemeEvent.blockNumber,
+          repAtCreation: await reputation.totalSupplyAt.call(schemeEvent.blockNumber),
+          winningVote: votingMachineProposalInfo.winningVote,
+          proposer: votingMachineProposalInfo.proposer,
+          currentBoostedVotePeriodLimit: votingMachineProposalInfo.currentBoostedVotePeriodLimit,
+          paramsHash: cache.schemes[schemeContract.address].paramsHash,
+          daoBountyRemain: votingMachineProposalInfo.daoBountyRemain,
+          daoBounty: votingMachineProposalInfo.daoBounty,
+          totalStakes: votingMachineProposalInfo.totalStakes,
+          confidenceThreshold: votingMachineProposalInfo.confidenceThreshold,
+          secondsFromTimeOutTillExecuteBoosted: votingMachineProposalInfo.secondsFromTimeOutTillExecuteBoosted,
+          submittedTime: new BigNumber(proposalTimes[0].toString()),
+          boostedPhaseTime: new BigNumber(proposalTimes[1].toString()),
+          preBoostedPhaseTime: new BigNumber(proposalTimes[2].toString()),
+          daoRedeemItsWinnings: votingMachineProposalInfo.daoRedeemItsWinnings,
+          status: status,
+          priority: priority,
+          boostTime: boostTime,
+          finishTime: finishTime,
+          shouldBoost: proposalShouldBoost,
+          positiveVotes: proposalStatusWithVotes[0],
+          negativeVotes: proposalStatusWithVotes[1],
+          preBoostedPositiveVotes: proposalStatusWithVotes[2],
+          preBoostedNegativeVotes: proposalStatusWithVotes[3],
+          positiveStakes: proposalStatusWithVotes[4],
+          negativeStakes: proposalStatusWithVotes[5]
+        };
+      }
+    }));
+    
+  }));
+  
+  // Get all votes, stakes, redeems and redeems rep form voting machine events
+  votingMachineEvents.map(async (votingMachineEvent) => {
     switch (votingMachineEvent.event) {
       case "Vote":
-        cache.votes.push({
+        const preBoosted = await web3.eth.getBlock(votingMachineEvent.blockNumber).timestamp <
+          cache.proposals[votingMachineEvent.returnValues._proposalId].boostTime;
+        cache.votingMachineEvents.votes.push({
           voter: votingMachineEvent.returnValues._voter,
           vote: votingMachineEvent.returnValues._vote,
           amount: votingMachineEvent.returnValues._reputation,
-          // TO DO: Get the boosted block of the proposal to know if is preBoosted vote
-          preBoosted: false,
+          preBoosted: preBoosted,
           proposalId: votingMachineEvent.returnValues._proposalId,
           tx: votingMachineEvent.transactionHash,
           block: votingMachineEvent.blockNumber,
@@ -285,7 +370,7 @@ async function main() {
         });
       break;
       case "Stake":
-      cache.stakes.push({
+      cache.votingMachineEvents.stakes.push({
           staker: votingMachineEvent.returnValues._staker,    
           vote: votingMachineEvent.returnValues._vote,
           amount: votingMachineEvent.returnValues._amount,
@@ -298,7 +383,7 @@ async function main() {
         });
       break;
       case "Redeem":
-        cache.redeems.push({
+        cache.votingMachineEvents.redeems.push({
           beneficiary: votingMachineEvent.returnValues._beneficiary,
           amount: votingMachineEvent.returnValues._amount,
           proposalId: votingMachineEvent.returnValues._proposalId,
@@ -309,7 +394,7 @@ async function main() {
         });
       break;
       case "RedeemRep":
-        cache.redeemsRep.push({
+        cache.votingMachineEvents.redeemsRep.push({
           beneficiary: votingMachineEvent.returnValues._beneficiary,
           amount: votingMachineEvent.returnValues._amount,
           proposalId: votingMachineEvent.returnValues._proposalId,
@@ -320,15 +405,22 @@ async function main() {
         });
       break;
     }
-  })
+  });
   
-  // TO DO: Order voting machine events ?
-    
-  console.log(votingMachineEvents.length)
-  console.log(masterWalletSchemeEvents.length)
-  console.log(quickWalletSchemeEvents.length)
-  console.log(controllerEvents.length)
-  console.log(permissionRegistryEvents.length)
+  // Store all the rep holders balances up to date
+  reputationEvents.map((reputationEvent) => {
+    switch (reputationEvent.event) {
+      case "Mint":
+        cache.daoInfo.repHolders[reputationEvent.returnValues._to] = 
+          !cache.daoInfo.repHolders[reputationEvent.returnValues._to] ? reputationEvent.returnValues._amount
+          : cache.daoInfo.repHolders[reputationEvent.returnValues._to].plus(reputationEvent.returnValues._amount)
+      break;
+      case "Burn":
+        cache.daoInfo.repHolders[reputationEvent.returnValues._to] =
+          cache.daoInfo.repHolders[reputationEvent.returnValues._to].plus(reputationEvent.returnValues._amount)
+      break;
+    }
+  })
   
   console.log(cache)
 } 
--- scripts/helpers/cache.js
+++ scripts/helpers/cache.js
@@ -1,5 +1,7 @@
+const hre = require("hardhat");
 const _ = require("lodash");
 const { BigNumber } = require('bignumber.js');
+const moment = require('moment');
 
 BigNumber.config({
   EXPONENTIAL_AT: [-100, 100],
@@ -47,4 +49,129 @@ const decodeSchemeParameters = function(rawParameters) {
     };
 }
 
-module.exports = { getEventsBetweenBlocks, sortEvents, decodeSchemeParameters };
+const getBlockTimeStamp = async function(blockNumber) {
+  return await hre.web3.eth.getBlock(blockNumber).timestamp;
+}
+
+const decodeStatus = function(
+  stateInVotingMachine,
+  stateInScheme,
+  submittedTime,
+  boostedPhaseTime,
+  preBoostedPhaseTime,
+  queuedVotePeriodLimit,
+  boostedVotePeriodLimit,
+  quietEndingPeriod,
+  preBoostedVotePeriodLimit,
+  shouldBoost
+) {
+  
+  switch (stateInVotingMachine) {
+    case "1":
+      return { 
+        status: "Expired in Queue", 
+        priority: 1, 
+        boostTime: 0, 
+        finishTime: 0
+      };
+    break;
+    case "2":
+      if (stateInScheme == "3")
+        return { 
+          status: "Executed", 
+          priority: 2,
+          boostTime: 0,
+          finishTime: 0
+        };
+      else if (stateInScheme == "2")
+        return { 
+          status: "Rejected", 
+          priority: 2,
+          boostTime: 0,
+          finishTime: 0
+        };
+      else return { 
+        status: "Passed", 
+        priority: 2,
+        boostTime: 0,
+        finishTime: 0
+      };
+    break;
+    case "3":
+      if (moment().unix() > submittedTime.plus(queuedVotePeriodLimit).toNumber()) {
+        return { 
+          status: "Expired in Queue",
+          priority: 1,
+          boostTime: 0,
+          finishTime: submittedTime.plus(queuedVotePeriodLimit)
+        };
+      } else {
+        return { 
+          status: "In Queue", 
+          priority: 3,
+          boostTime: 0,
+          finishTime: submittedTime.plus(queuedVotePeriodLimit)
+        };
+      }
+    break;
+    case "4":
+      if (moment().unix() > preBoostedPhaseTime.plus(preBoostedVotePeriodLimit).toNumber() && shouldBoost) {
+        return { 
+          status: "Pending Boost", 
+          priority: 5,
+          boostTime: boostedPhaseTime,
+          finishTime: bnum(moment().unix()).plus(boostedVotePeriodLimit)
+        };
+      } else if (moment().unix() > preBoostedPhaseTime.plus(preBoostedVotePeriodLimit).plus(boostedVotePeriodLimit).toNumber() && shouldBoost) {
+        return { 
+          status: "Expired in Queue", 
+          priority: 1,
+          boostTime: boostedPhaseTime,
+          finishTime: bnum(moment().unix()).plus(boostedVotePeriodLimit)
+        };
+      } else {
+        return { 
+          status: "Pre Boosted", 
+          priority: 4,
+          boostTime: preBoostedPhaseTime.plus(preBoostedVotePeriodLimit),
+          finishTime: preBoostedPhaseTime.plus(preBoostedVotePeriodLimit).plus(boostedVotePeriodLimit), 
+        };
+      }
+    break;
+    case "5":
+      if (moment().unix() > boostedPhaseTime.plus(boostedVotePeriodLimit).toNumber()) {
+        return { 
+          status: "Pending Execution", 
+          priority: 6,
+          boostTime: 0,
+          finishTime: 0
+        };
+      } else {
+        return { 
+          status: "Boosted", 
+          priority: 5,
+          boostTime: 0,
+          finishTime: boostedPhaseTime.plus(boostedVotePeriodLimit)
+        };
+      }
+    break;
+    case "6":
+      return { 
+        status: "Quiet Ending Period", 
+        priority: 6,
+        boostTime: 0,
+        finishTime: boostedPhaseTime.plus(quietEndingPeriod)
+      };
+    break;
+    default:
+    return { 
+      status: "", 
+      priority: 0,
+      boostTime: 0,
+      finishTime: 0
+    };
+  }
+}
+
+
+module.exports = { getEventsBetweenBlocks, sortEvents, decodeSchemeParameters, getBlockTimeStamp, decodeStatus };
