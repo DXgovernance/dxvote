f803479c9c06ba7038bebf98bba9805489b422be
AugustoL <me@augustol.com>
2021-06-23 16:40:29 -0300

    feat(src/ProposalTemplates): Update proposal templates

--- src/config/proposalTemplates.ts
+++ src/config/proposalTemplates.ts
@@ -1,48 +1,71 @@
 
 const ProposalTemplates = [
     {
-      name: "Worker Proposal",
-      title: "Worker Proposal of WORKER_ALIAS from START_DAY to FINISH_DAY",
-      description: 
-`## Timeframe
-
-## Responsabilities
-
-## Compensation`
-    },{
       name: "Contributor Proposal",
-      title: "Contributor Proposal of WORKER_ALIAS for MAIN_GOAL",
+      title: "[NAME] Contributor Proposal [DATE - DATE]",
       description: 
-`## Timeframe
+`# Background
+  - If this is your first contributor proposal, state any context for making the proposal, including:
+    - What position you will be filling – i.e. developer, marketing, governance, etc.
+    - Who you have been in contact with regarding the position (add their DAOtalk usernames)
+  - If you are a continuing contributor, state which products and areas you are working on / any other relevant background information
+# Timeframe
+  - State the date range, typically 2 months (2.5 months for first-time workers)
+# Scope of Contribution
+  - **Goals** :
+    - State the products and /or strategy areas you will be working under. E.g. Omen and Governance. Include both short-term goals that you seek to accomplish during the working period and long-term goals.
+  - **Responsibilities:**
+    - List the specific deliverables you will be either completing and / or working on during the working period. Try to state which deliverables you intend to complete within the period and which are long-term or ongoing projects.
 
-## Goals
-
-## Compensation`
+# Reflection
+- If You Are A Returning Contributor, Write a Self-Reflection
+  - State what you did well, what you did okay, and what you need to improve on
+# Compensation
+  - Experience Level : 1-5
+  - Time commitment :  Percent (%) of time
+  - [ETH/DAI AMT] : For 2 months of work at level \_, to be sent half on the first payment proposal and half on the second payment proposal
+  - [DXD AMT] : Vested for 2 years with a 1 year cliff starting the [DATE WORK PERIOD STARTS]
+  - [% AMT REP] : For 2 months of work , to be sent half on the first payment proposal and half on the second payment proposal
+# Work Experience
+  - Prior DXdao work proposals (if applicable)
+  - Past Work for DXdao
+    - If a developer, Github links
+    - If a non-tech worker, doc links or described deliverables
+  - If new, past relevant work experience
+    - Developers : Github profile link
+    - Non-tech : suggested Linkedin / other references`
     },{
-      name: "Worker Paymnet Proposal",
-      title: "Worker Paymnet Proposal of WORKER_ALIAS from START_DAY to FINISH_DAY",
+      name: "Payment Proposal",
+      title: "[NAME] Payment Proposal [DATE - DATE]",
       description: 
-`## Timeframe
-
-## Responsabilities
-
-## Proof of Work
-
-## Payment`
-    },{
-      name: "Contributor Paymnet Proposal",
-      title: "Contributor Paymnet Proposal of WORKER_ALIAS from START_DAY to FINISH_DAY",
-      description: 
-`## Timeframe
-
-## Goals
-
-## Proof of Work
-
-## Payment`
+`# Timeframe
+  - State the contributor proposal date range
+# Background
+  - State the context for making the contributor payment proposal, including:
+    - What position you will be filling – i.e. developer, marketing, governance, etc.
+    - Proposed Scope of Contribution
+    - Self-Reflection on Scope of Contribution
+    - Include the URL to the original proposal forum post
+# Deliverables
+- Include information around deliverables that were completed during your worker period
+    
+# Compensation
+  - Experience Level : 1-5
+  - Time commitment : Percent (%) of time
+  - [ETH/DAI AMT] : For second half payment proposal
+  - [DXD AMT] : Vested for 2 years with a 1 year cliff starting the [DATE WORK PERIOD STARTS]
+  - [% AMT REP]: For 2 second half payment proposal
+# Work Experience
+  - Prior DXdao work proposals (if applicable)
+  - Past Work for DXdao
+    - If a developer, Github links
+    - If a non-tech worker, doc links or described 
+  - If new, past relevant work experience
+    - Developers : Github profile link
+    - Non-tech : suggested Linkedin / other references`
     },{
       name: "Signal Proposal",
-      title: "Signal Proposal for DAO_ACTION",
+      title: "Signal Proposal for [DAO_ACTION]",
       description: 
 `## Description
 
