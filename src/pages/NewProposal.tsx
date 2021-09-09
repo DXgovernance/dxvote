29da24687f5e03ee10cfcf1ce2004f5c38a23671
AugustoL <me@augustol.com>
2021-09-09 10:42:30 -0300

    fix(src/NewProposal): Fix new proposal form loading for contribution reward

--- src/pages/NewProposal.tsx
+++ src/pages/NewProposal.tsx
@@ -494,7 +494,7 @@ const NewProposalPage = observer(() => {
         setCallsInState(calls);
       }
     }
-    if (calls[0] && calls[0].allowedFunctions.length == 0)
+    if (calls[0] && calls[0].allowedFunctions.length == 0 && allowedToCall[0])
       onToSelectChange(0, allowedToCall[0].value)
 
     return (
