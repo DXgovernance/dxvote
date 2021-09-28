933743ace3babf2155dccbc04bf666d4a0d81699
Hamza <hamza.h.hamud@gmail.com>
2021-09-28 18:16:00 +0100

    fix: added better comments

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,10 +8,11 @@ export function mapEnum(enumerable: EnumType, fn: Function): any[] {
   // get all the members of the enum
   let enumMembers: any[] = Object.keys(enumerable).map(key => enumerable[key]);
 
+  // enum contains both string and number representations of the enum ["None", 0]
   // we are only interested in the numeric identifiers as these represent the values
   let enumValues: number[] = enumMembers.filter(v => typeof v === 'number');
 
-  // now map through the enum values
+  // now map through the enum number values
   return enumValues.map(m => fn(m));
 }
 
