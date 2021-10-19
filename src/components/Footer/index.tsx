3ab2ab8776a9592e95cc5b8da38be8df7f6ed241
luchux <lucianoguasco@gmail.com>
2021-10-18 22:48:54 -0700

    formatting as same as footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -98,15 +98,13 @@ const Footer = () => {
         </FooterItem>
         <FooterDivider></FooterDivider>
         <FooterItem>
-          <small>
-            v
-            {`${process.env.REACT_APP_VERSION}${
-              process.env.NODE_ENV !== 'production'
-                ? `-${process.env.NODE_ENV}-${process.env.REACT_APP_GIT_SHA}`
-                : ''
-            }`}
-            {process.env.REACT_APP_GIT_SHA}
-          </small>
+          v
+          {`${process.env.REACT_APP_VERSION}${
+            process.env.NODE_ENV !== 'production'
+              ? `-${process.env.NODE_ENV}-${process.env.REACT_APP_GIT_SHA}`
+              : ''
+          }`}
+          {process.env.REACT_APP_GIT_SHA}
         </FooterItem>
       </LeftFooter>
     </FooterWrapper>
