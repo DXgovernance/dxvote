f763fc4ac44206719d7f5e4c83432a3fdd2adc61
AugustoL <me@augustol.com>
2021-10-29 13:40:26 +0100

    refactor(src/Pages/NewProposalTypes): Change style of new proposal type box

--- src/pages/NewProposalType.tsx
+++ src/pages/NewProposalType.tsx
@@ -5,6 +5,7 @@ import { observer } from 'mobx-react';
 import { useHistory } from 'react-router-dom';
 
 import { useContext } from '../contexts';
+import { Box } from '../components/common';
 
 const VerticalLayout = styled.div`
   display: flex;
@@ -46,19 +47,18 @@ const OptionsWrapper = styled.div`
   justify-content: space-around;
 `;
 
-const ProposalType = styled.div`
+const ProposalType = styled(Box)`
   width: 10%;
-  min-width: 100px;
+  min-width: 120px;
   height: 100px;
-  box-shadow: 0px 0px 15px 11px #3a58fd47;
   margin: 5%;
   padding: 10px;
   cursor: pointer;
-  transition-duration: 1s;
-  :hover {
-    box-shadow: 0px 0px 20px 15px #3a57fd66;
-    transform: scale(1.05);
-  }
+  text-align: center;
+  color: var(--dark-text-gray);
+  padding: 5px 10px;
+  font-weight: 500;
+  font-size: 24px;
 `;
 
 export const NewProposalTypePage = observer(() => {
