e491da1f45d1c986f9f2f3d34ec13c4c0ad9eaa3
Hamza <hamza.h.hamud@gmail.com>
2021-11-16 12:28:35 +0000

    fix: removed file and imported styled component

--- src/components/Proposals/Search/StatusSearch.tsx
+++ src/components/Proposals/Search/StatusSearch.tsx
@@ -1,26 +1,9 @@
 import { useProposals } from 'hooks/useProposals';
 import { useState, useEffect} from 'react';
 import { enumKeys, VotingMachineProposalState } from 'utils';
-import styled from 'styled-components';
 import { useParams } from 'hooks/useSearch';
+import { ProposalsFilter } from './style';
 
-const ProposalsFilter = styled.select`
-  background-color: ${props => props.color || '#536DFE'};
-  border-radius: 4px;
-  color: white;
-  height: 34px;
-  letter-spacing: 1px;
-  font-weight: 500;
-  line-height: 34px;
-  text-align: center;
-  cursor: pointer;
-  width: 200px;
-  padding: 0px 10px;
-  margin: 10px 0px;
-  font-family: var(--roboto);
-  border: 0px;
-  align-self: center;
-`;
 
 const StatusSearch = () => {
   const [{filters: {scheme, search}}, dispatch] = useProposals();
