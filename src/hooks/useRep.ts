983f4676af45e47bafa2cf5f129d3a4fc7994c3d
Hamza <hamza.h.hamud@gmail.com>
2021-11-22 11:35:23 +0000

    fix: initial rendering error

--- src/hooks/useRep.ts
+++ src/hooks/useRep.ts
@@ -1,5 +1,6 @@
 import { useContext } from 'contexts';
 import { useState, useEffect } from 'react';
+import { bnum } from 'utils';
 import { BigNumber } from 'utils/bignumber';
 import { ZERO_ADDRESS } from 'utils/constants';
 
@@ -16,45 +17,57 @@ interface MemoInteface {
 
 export const useRep = (userAddress: string = ZERO_ADDRESS): UseRepReturns => {
   const {
-    context: { daoStore, configStore },
+    context: { daoStore, configStore , providerStore},
   } = useContext();
   const [memo, setMemo] = useState<MemoInteface>({});
-  const cache: MemoInteface = {};
   const repEvents = daoStore.getCache().daoInfo.repEvents;
   const inL2 = configStore.getActiveChainName().indexOf('arbitrum') > -1;
 
-  let totalSupply: BigNumber;
-  let userRep: BigNumber;
+  let totalSupply: BigNumber = bnum(0)
+  let userRep: BigNumber = bnum(0)
+
+
 
   const getRep = (atBlock: number = 0) => {
-    const totalSupply = memo[atBlock].totalSupply;
-    const userRep = memo[atBlock].userRep;
+    const values = memo
+    if (atBlock === 0) atBlock = providerStore.getCurrentBlockNumber();
+    const closest = Object.keys(values).reduce((a,b) => parseInt(b) > atBlock ? a : b)
+    const totalSupply = values[closest]["totalSupply"];
+    const userRep = values[closest]["userRep"];
 
     return { totalSupply, userRep };
   };
 
+  useEffect(() => {
   for (let i = 0; i < repEvents.length; i++) {
     if (repEvents[i].event === 'Mint') {
       totalSupply = totalSupply.plus(repEvents[i].amount);
-      cache[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber]
-        .totalSupply == totalSupply;
+      memo[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber] =  {
+        "totalSupply": totalSupply,
+        "userRep" : bnum(0)
+      }
       if (repEvents[i].account === userAddress)
         userRep = userRep.plus(repEvents[i].amount);
-      cache[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber]
-        .userRep == userRep;
+      memo[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber] = {
+        "totalSupply": totalSupply,
+        "userRep": userRep
+      }
+      memo[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber]["userRep"] = userRep;
     } else if (repEvents[i].event === 'Burn') {
       totalSupply = totalSupply.minus(repEvents[i].amount);
-      cache[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber]
-        .totalSupply == totalSupply;
+      memo[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber] = {
+        "totalSupply": totalSupply,
+        "userRep": bnum(0)
+      }
       if (repEvents[i].account === userAddress)
         userRep = userRep.minus(repEvents[i].amount);
-      cache[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber]
-        .userRep == userRep;
+      memo[inL2 ? repEvents[i].l2BlockNumber : repEvents[i].l1BlockNumber] = {
+        "totalSupply": totalSupply,
+        "userRep": userRep
+      }
     }
   }
-
-  useEffect(() => {
-    setMemo(cache);
+    setMemo(memo);
   }, [repEvents]);
 
   return {
