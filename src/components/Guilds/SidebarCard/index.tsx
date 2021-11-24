922a01d8ec278d0cec65c244167f61eee76ee96e
Madusha Prasanjith <hi@madusha.me>
2021-11-24 17:22:50 +0530

    Add Proposal page sidebar styles.

---
+++ src/components/Guilds/SidebarCard/index.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { Box } from '../common/Layout';
+
+const CardWrapper = styled(Box)`
+  border: 1px solid #000;
+  border-radius: 0.5rem;
+  margin-bottom: 1rem;
+`;
+
+const CardHeader = styled(Box)`
+  border-bottom: 1px solid #000;
+`;
+
+interface SidebarCardProps {
+  header?: JSX.Element;
+  children?: JSX.Element;
+}
+
+const SidebarCard: React.FC<SidebarCardProps> = ({ header, children }) => {
+  return (
+    <CardWrapper>
+      {header && <CardHeader>{header}</CardHeader>}
+      {children}
+    </CardWrapper>
+  );
+};
+
+export default SidebarCard;
