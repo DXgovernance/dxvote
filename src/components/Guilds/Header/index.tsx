cf19ec856b2162b7935c5b0ead4b1023a99dbf97
Madusha Prasanjith <hi@madusha.me>
2021-11-26 12:46:38 +0530

    feat: Add Heading component and update theme.

--- src/components/Guilds/Header/index.tsx
+++ src/components/Guilds/Header/index.tsx
@@ -4,6 +4,7 @@ import { Box, Container } from '../common/Layout';
 import dxIcon from '../../../assets/images/dxdao-icon.svg';
 import ethIcon from '../../../assets/images/ethereum.svg';
 import { IconButton } from '../common/Button';
+import { Heading } from '../common/Typography';
 
 const HeaderWrapper = styled(Box)`
   padding: 0.75rem 0;
@@ -28,11 +29,6 @@ const MenuItems = styled(Box)`
   margin-left: auto;
 `;
 
-const AppLogo = styled(Box)`
-  font-weight: 600;
-  font-size: 1.25rem;
-`;
-
 const ButtonIcon = styled.img`
   height: 1.1rem;
   width: 1.1rem;
@@ -42,7 +38,7 @@ const Header = () => {
   return (
     <HeaderWrapper as="header">
       <HeaderContainer>
-        <AppLogo>DXvote</AppLogo>
+        <Heading size={2}><strong>DXvote</strong></Heading>
         <MenuItems>
           <IconButton iconLeft>
             <ButtonIcon src={ethIcon} alt={'Icon'} />
