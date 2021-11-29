1a40b8286dfae851d13039cc21ec0b8d151ffaf7
rossneilson <rossneilson99@gmail.com>
2021-11-29 17:41:53 +0000

    feat: Updating blok numbers

--- scripts/buildCache.sh
+++ scripts/buildCache.sh
@@ -12,10 +12,10 @@ echo "${contents}" > tsconfig.json
 # Run build cache in all networks
 # Use RESET_CACHE=1 to reset the cache and ignore existing content
 # Use EMPTY_CACHE=1 to save an empty cache
-CACHE_TO_BLOCK=13708329 yarn hardhat run --network mainnet scripts/buildCache.ts
-CACHE_TO_BLOCK=19318266 yarn hardhat run --network xdai scripts/buildCache.ts
+CACHE_TO_BLOCK=13709919 yarn hardhat run --network mainnet scripts/buildCache.ts
+CACHE_TO_BLOCK=19322758 yarn hardhat run --network xdai scripts/buildCache.ts
 CACHE_TO_BLOCK=3430998 yarn hardhat run --network arbitrum scripts/buildCache.ts
-CACHE_TO_BLOCK=9726830 yarn hardhat run --network rinkeby scripts/buildCache.ts
+CACHE_TO_BLOCK=9629062 yarn hardhat run --network rinkeby scripts/buildCache.ts
 CACHE_TO_BLOCK=6264591 yarn hardhat run --network arbitrumTestnet scripts/buildCache.ts
 yarn prettier --write src/configs/**/*.json
 sleep 1
