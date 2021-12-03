f4f395d4a21673633e2bd25e354f665140af57b0
Tiago Neto <tiago.f.neto@gmail.com>
2021-12-03 17:37:05 +0000

    feat: Error toast show modal button

--- src/components/ErrorToast.tsx
+++ src/components/ErrorToast.tsx
@@ -1,10 +1,19 @@
+import styled from 'styled-components';
 import { toast } from 'react-toastify';
+import { Button } from './common';
+
+export const ErrorToastButton = styled(Button)`
+  background-color: #e74c3c; 
+`;
 
 export default function ErrorToast(
   content) {
-  return toast.error(content, {
+  return toast.error(<div>
+    {content}
+    <ErrorToastButton>More</ErrorToastButton>
+  </div>, {
 position: "bottom-right",
-autoClose: 5000,
+autoClose: 50000,
 hideProgressBar: false,
 closeOnClick: true,
 pauseOnHover: true,
