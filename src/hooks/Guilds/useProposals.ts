95d1152514caa1f1fcf479b321b342d9ba13e6f4
Hamza <hamza.h.hamud@gmail.com>
2021-12-08 20:47:54 +0000

    fix: use typechain in hook && create interfaces

--- src/hooks/Guilds/useProposals.ts
+++ src/hooks/Guilds/useProposals.ts
@@ -1,36 +1,64 @@
-import { useEffect, useState } from "react"
-import useContract from '../contracts/useContract';
-
+import { Signer } from 'ethers';
+import { Provider } from 'ethers/providers';
+import { BigNumber } from 'ethers/utils';
+import { useEffect, useState } from 'react';
+import { ERC20Guild__factory } from '../../types/factories/ERC20Guild__factory';
 
 export interface useProposalsReturns {
-    proposals: Proposal[]
-
+    proposals: GuildsProposal[];
 }
 
-//
-// @TODO use typechain interfaces for state
-// @TODO get contract addresses using
-//
+export interface useProposalParams {
+    contractAddress: string,
+    signerOrProvider: Signer | Provider
+}
 
-export const useProposals = (contractAddress: String): useProposalsReturns => {
-    const [proposals, setProposals] = useState<Proposal[]>([])
-    const [proposalIds, setProposalIds] = useState()
+export interface GuildsProposal {
+    creator: string;
+    startTime: BigNumber;
+    endTime: BigNumber;
+    to: string[];
+    data: string[];
+    value: BigNumber[];
+    totalActions: BigNumber;
+    title: string;
+    contentHash: string;
+    state: number;
+    totalVotes: BigNumber[];
+}
 
+export const useProposals = ({ contractAddress, signerOrProvider }: useProposalParams): useProposalsReturns => {
+    const [proposals, setProposals] = useState<GuildsProposal[]>([]);
+    const [proposalIds, setProposalIds] = useState<string[]>([]);
 
-    const contract = useContract()
+    const ERC20_GUILD_INSTANCE = ERC20Guild__factory.connect(
+        contractAddress,
+        signerOrProvider
+    );
+
 
     useEffect(() => {
-        setProposals([])
+        const getProposals = async () => {
+            const proposals = await Promise.all(proposalIds.map(id => ERC20_GUILD_INSTANCE.getProposal(id)))
+            return setProposals(proposals);
+        }
 
-    }, [])
+        getProposals()
+
+    }, [contractAddress, signerOrProvider]);
 
     useEffect(() => {
-        setProposalIds()
+        const getProposalIds = async () => {
+            const ids = await ERC20_GUILD_INSTANCE.getProposalsIds();
+            return setProposalIds(ids);
+        };
 
-    }, [])
+        getProposalIds();
+
+
+    }, [contractAddress, signerOrProvider]);
 
     return {
-        proposals
-
-    }
-}
+        proposals,
+    };
+};
