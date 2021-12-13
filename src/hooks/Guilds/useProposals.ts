90cb1f946673621b42d56ca4f175902f05a5ceaa
Hamza <hamza.h.hamud@gmail.com>
2021-12-13 10:44:25 +0000

    fix: create loading and error states

--- src/hooks/Guilds/useProposals.ts
+++ src/hooks/Guilds/useProposals.ts
@@ -1,64 +1,84 @@
-import { Signer } from 'ethers';
-import { Provider } from 'ethers/providers';
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from 'ethers/providers';
 import { BigNumber } from 'ethers/utils';
 import { useEffect, useState } from 'react';
 import { ERC20Guild__factory } from '../../types/factories/ERC20Guild__factory';
 
 export interface useProposalsReturns {
-    proposals: GuildsProposal[];
+  proposals: GuildsProposal[];
+  error: null | Error;
+  loading: boolean;
 }
 
-export interface useProposalParams {
-    contractAddress: string,
-    signerOrProvider: Signer | Provider
-}
 
 export interface GuildsProposal {
-    creator: string;
-    startTime: BigNumber;
-    endTime: BigNumber;
-    to: string[];
-    data: string[];
-    value: BigNumber[];
-    totalActions: BigNumber;
-    title: string;
-    contentHash: string;
-    state: number;
-    totalVotes: BigNumber[];
+  creator: string;
+  startTime: BigNumber;
+  endTime: BigNumber;
+  to: string[];
+  data: string[];
+  value: BigNumber[];
+  totalActions: BigNumber;
+  title: string;
+  contentHash: string;
+  state: number;
+  totalVotes: BigNumber[];
 }
 
-export const useProposals = ({ contractAddress, signerOrProvider }: useProposalParams): useProposalsReturns => {
-    const [proposals, setProposals] = useState<GuildsProposal[]>([]);
-    const [proposalIds, setProposalIds] = useState<string[]>([]);
+export const useProposals = (
+  contractAddress: string): useProposalsReturns => {
+  const [proposals, setProposals] = useState<GuildsProposal[]>([]);
+  const [proposalIds, setProposalIds] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const ERC20_GUILD_INSTANCE = ERC20Guild__factory.connect(
-        contractAddress,
-        signerOrProvider
-    );
+  const { library } = useWeb3React();
+  const provider = new Web3Provider(library.currentProvider);
 
+  const ERC20_GUILD_INSTANCE = ERC20Guild__factory.connect(
+    contractAddress,
+    provider
+  );
 
-    useEffect(() => {
-        const getProposals = async () => {
-            const proposals = await Promise.all(proposalIds.map(id => ERC20_GUILD_INSTANCE.getProposal(id)))
-            return setProposals(proposals);
-        }
-
-        getProposals()
-
-    }, [contractAddress, signerOrProvider]);
-
-    useEffect(() => {
-        const getProposalIds = async () => {
-            const ids = await ERC20_GUILD_INSTANCE.getProposalsIds();
-            return setProposalIds(ids);
-        };
-
-        getProposalIds();
-
-
-    }, [contractAddress, signerOrProvider]);
-
-    return {
-        proposals,
+  useEffect(() => {
+    if (!library) return null
+    setLoading(true);
+    const getProposals = async () => {
+      const proposals = await Promise.all(
+        proposalIds.map(id => ERC20_GUILD_INSTANCE.getProposal(id))
+      );
+      return setProposals(proposals);
     };
+
+    try {
+      getProposals();
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }, [contractAddress, library]);
+
+  useEffect(() => {
+    if (!library) return null
+    setLoading(true);
+    const getProposalIds = async () => {
+      const ids = await ERC20_GUILD_INSTANCE.getProposalsIds();
+      return setProposalIds(ids);
+    };
+
+    try {
+      getProposalIds();
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }, [contractAddress, provider]);
+
+  return {
+    proposals,
+    loading,
+    error,
+  };
 };
