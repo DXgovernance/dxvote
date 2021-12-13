a70ae8db6c39e98ac2d36a6f1181d0422ca848df
Hamza <hamza.h.hamud@gmail.com>
2021-12-13 11:47:14 +0000

    fix: refactor useContract into a hook

--- src/hooks/Guilds/contracts/useContract.ts
+++ src/hooks/Guilds/contracts/useContract.ts
@@ -1,15 +1,32 @@
+import { Contract } from '@ethersproject/contracts'
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from 'ethers/providers';
+import { useMemo } from 'react';
 import { getContract } from '../../../utils/contracts';
+import ERC20Guild_ABI from '../../../abis/ERC20Guild.json'
+//import { ERC20Guild } from '../../../types/ERC20Guild'
 
-export default function useContract(
+export default function useContract<T extends Contract = Contract>(
   contractId: string,
   abi: any,
   web3Context?: string
-) {
-  const { library } = useWeb3React(web3Context);
-  const provider = new Web3Provider(library.currentProvider);
-  const contract = getContract(contractId, abi, provider);
+): T | null {
 
-  return contract;
+  const { library } = useWeb3React(web3Context);
+  return useMemo(() => {
+    if (!library) return null
+    try {
+      const provider = new Web3Provider(library.currentProvider);
+      const contract = getContract(contractId, abi, provider);
+      return contract;
+    } catch (e) {
+      console.error(e)
+      return null
+    }
+  }, [contractId, abi, web3Context, library]) as T
 }
+
+export function useERC20Guild(contractId: string, web3Context?: string) {
+  return useContract(contractId, ERC20Guild_ABI.abi, web3Context)
+}
+
