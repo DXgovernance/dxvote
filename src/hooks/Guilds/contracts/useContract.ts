9384fa1f57d318127379257f759d7a6fdbacb932
Hamza <hamza.h.hamud@gmail.com>
2021-12-13 12:36:04 +0000

    fix: cleanup and removal of extra useEffect

--- src/hooks/Guilds/contracts/useContract.ts
+++ src/hooks/Guilds/contracts/useContract.ts
@@ -1,32 +1,30 @@
-import { Contract } from '@ethersproject/contracts'
+import { Contract } from 'ethers/contract';
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from 'ethers/providers';
 import { useMemo } from 'react';
 import { getContract } from '../../../utils/contracts';
-import ERC20Guild_ABI from '../../../abis/ERC20Guild.json'
-//import { ERC20Guild } from '../../../types/ERC20Guild'
+import ERC20Guild_ABI from '../../../abis/ERC20Guild.json';
+import { ERC20Guild } from '../../../types/ERC20Guild';
 
-export default function useContract<T extends Contract = Contract>(
+export default function useContract<T extends Contract>(
   contractId: string,
   abi: any,
   web3Context?: string
 ): T | null {
-
   const { library } = useWeb3React(web3Context);
   return useMemo(() => {
-    if (!library) return null
+    if (!library) return null;
     try {
       const provider = new Web3Provider(library.currentProvider);
       const contract = getContract(contractId, abi, provider);
       return contract;
     } catch (e) {
-      console.error(e)
-      return null
+      console.error(e);
+      return null;
     }
-  }, [contractId, abi, web3Context, library]) as T
+  }, [contractId, abi, web3Context, library]) as unknown as T;
 }
 
 export function useERC20Guild(contractId: string, web3Context?: string) {
-  return useContract(contractId, ERC20Guild_ABI.abi, web3Context)
+  return useContract<ERC20Guild>(contractId, ERC20Guild_ABI.abi, web3Context);
 }
-
--- src/hooks/Guilds/useProposals.ts
+++ src/hooks/Guilds/useProposals.ts
@@ -1,15 +1,12 @@
-import { useWeb3React } from '@web3-react/core';
-import { Web3Provider } from 'ethers/providers';
 import { BigNumber } from 'ethers/utils';
 import { useEffect, useState } from 'react';
-import { ERC20Guild__factory } from '../../types/factories/ERC20Guild__factory';
+import { useERC20Guild } from './contracts/useContract';
 
 export interface useProposalsReturns {
   proposals: GuildsProposal[];
   error: null | Error;
   loading: boolean;
 }
-
 
 export interface GuildsProposal {
   creator: string;
@@ -25,56 +22,37 @@ export interface GuildsProposal {
   totalVotes: BigNumber[];
 }
 
-export const useProposals = (
-  contractAddress: string): useProposalsReturns => {
+export const useProposals = (contractAddress: string): useProposalsReturns => {
   const [proposals, setProposals] = useState<GuildsProposal[]>([]);
   const [proposalIds, setProposalIds] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const { library } = useWeb3React();
-  const provider = new Web3Provider(library.currentProvider);
-
-  const ERC20_GUILD_INSTANCE = ERC20Guild__factory.connect(
-    contractAddress,
-    provider
-  );
+  const contract = useERC20Guild(contractAddress);
 
   useEffect(() => {
-    if (!library) return null
     setLoading(true);
+    if (!contract) return null;
+    const getProposalIds = async () => {
+      const ids = await contract.getProposalsIds();
+      return setProposalIds(ids);
+    };
+
     const getProposals = async () => {
       const proposals = await Promise.all(
-        proposalIds.map(id => ERC20_GUILD_INSTANCE.getProposal(id))
+        proposalIds.map(id => contract.getProposal(id))
       );
       return setProposals(proposals);
     };
-
     try {
+      getProposalIds();
       getProposals();
     } catch (e) {
       setError(e);
     } finally {
       setLoading(false);
     }
-  }, [contractAddress, library]);
-
-  useEffect(() => {
-    if (!library) return null
-    setLoading(true);
-    const getProposalIds = async () => {
-      const ids = await ERC20_GUILD_INSTANCE.getProposalsIds();
-      return setProposalIds(ids);
-    };
-
-    try {
-      getProposalIds();
-    } catch (e) {
-      setError(e);
-    } finally {
-      setLoading(false);
-    }
-  }, [contractAddress, provider]);
+  }, [contractAddress, contract]);
 
   return {
     proposals,
