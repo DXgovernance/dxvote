6cd02ac8fad940a9368b75486325229043e7db60
Hamza <hamza.h.hamud@gmail.com>
2021-12-13 15:17:13 +0000

    fix: add typechain to build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@
 export REACT_APP_GIT_SHA=$(echo $(git rev-parse  HEAD) | cut -c1-9)
 export REACT_APP_VERSION=$npm_package_version
 export SKIP_PREFLIGHT_CHECK=true
+yarn typechain
 yarn react-app-rewired build
