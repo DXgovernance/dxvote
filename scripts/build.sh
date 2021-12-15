8a0fc571468024d69a9ebb8abce0dbd8d8b11547
Hamza <hamza.h.hamud@gmail.com>
2021-12-15 16:17:55 +0000

    fix: remove typechain from builds.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,4 @@
 export REACT_APP_GIT_SHA=$(echo $(git rev-parse  HEAD) | cut -c1-9)
 export REACT_APP_VERSION=$npm_package_version
 export SKIP_PREFLIGHT_CHECK=true
-yarn typechain
 yarn react-app-rewired build
