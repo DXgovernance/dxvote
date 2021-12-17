6a6fb18f6505ba9b262d03c8b2cbb75f3c70cbb3
Milton Tulli <miltontulli@Miltons-MBP.fibertel.com.ar>
2021-12-17 13:03:40 -0300

    change arg type

--- src/utils/encodingCalls.ts
+++ src/utils/encodingCalls.ts
@@ -64,7 +64,7 @@ export const encodeDxdVestingRelease = (library, token) => {
     library.eth.abi.encodeFunctionSignature('release(address)');
 
   const vestingParamsEncoded = library.eth.abi
-    .encodeParameters(['string'], [token])
+    .encodeParameters(['address'], [token])
     .substring(2);
 
   return vestingFunctionEncoded + vestingParamsEncoded;
