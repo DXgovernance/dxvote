269d730f8d11cab8efc7d8aee6de5b466beaf33b
rossneilson <rossneilson99@gmail.com>
2021-12-30 21:05:17 +0000

    fix: Updating another bignumber import to use util

--- src/hooks/ContributorProposal/useSubmitProposal.ts
+++ src/hooks/ContributorProposal/useSubmitProposal.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import BigNumber from 'bignumber.js';
+import { BigNumber } from '../../utils/bignumber';
 import {
   encodeDxdVestingCreate,
   encodeErc20Approval,
