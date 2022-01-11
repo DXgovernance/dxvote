808352936dd723ae7c784b9767a58933252a7afa
Milton <milton.tulli@gmail.com>
2022-01-11 20:50:18 -0300

    Refactor config context to use functional component

--- src/contexts/Guilds/config.tsx
+++ src/contexts/Guilds/config.tsx
@@ -3,13 +3,76 @@ import { providers, BigNumber } from 'ethers';
 import { useWeb3React } from '@web3-react/core';
 import { Web3ReactContextInterface } from '@web3-react/core/dist/types';
 import { matchPath } from 'react-router';
-import { getContract } from '../../utils/contracts';
+import { getContract as getContractUtil } from '../../utils/contracts';
 import { abi as ERC20Guild_ABI } from '../../contracts/ERC20Guild.json';
 import { ERC20Guild } from '../../types/ERC20Guild';
 
-export interface ConfigContext extends GuildServiceProviderState {
-  initialize: (context: Web3ReactContextInterface, address: string) => void;
-}
+const getContract = (
+  web3Context: Web3ReactContextInterface,
+  address: string
+): ERC20Guild | null => {
+  try {
+    const currentProvider = web3Context?.library?.currentProvider;
+    if (!currentProvider) {
+      console.debug(
+        '[GuildServiceContext - _getContract]: currentProvider null'
+      );
+      return null;
+    }
+    const provider = new providers.Web3Provider(currentProvider);
+    const contract = getContractUtil(
+      address,
+      ERC20Guild_ABI,
+      provider
+    ) as ERC20Guild;
+    return contract;
+  } catch (e) {
+    console.error(
+      '[GuildServiceContext - _getContract]: Error getting contract instance',
+      e
+    );
+    return null;
+  }
+};
+
+const getGuildConfig = async (contract: ERC20Guild) => {
+  if (!contract) {
+    console.debug(
+      '[GuildServiceContext - updateGuildConfig]: No contract found'
+    );
+    return {};
+  }
+  try {
+    const [
+      token,
+      permissionRegistry,
+      name,
+      proposalTime,
+      timeForExecution,
+      maxActiveProposals,
+    ] = await Promise.all([
+      contract.getToken(),
+      contract.getPermissionRegistry(),
+      contract.getName(),
+      contract.getProposalTime(),
+      contract.getTimeForExecution(),
+      contract.getMaxActiveProposals(),
+    ]);
+    return {
+      token,
+      permissionRegistry,
+      name,
+      proposalTime,
+      timeForExecution,
+      maxActiveProposals,
+    };
+  } catch (e) {
+    console.error('[GuildProviderService - updateGuildConfig]', e);
+    return {};
+  }
+};
+
+export interface ConfigContext extends GuildServiceProviderState {}
 
 export const GuildConfigContext = React.createContext<ConfigContext>(null);
 
@@ -24,18 +87,16 @@ interface GuildServiceProviderState {
   timeForExecution: BigNumber;
   maxActiveProposals: BigNumber;
 }
-
-interface GuildServiceProviderProps {
+interface GuildConfigProviderProps {
   children: React.ReactNode;
-  web3Context: Web3ReactContextInterface;
-  address: string;
 }
 
-class GuildService extends React.Component<
-  GuildServiceProviderProps,
-  GuildServiceProviderState
-> {
-  state: GuildServiceProviderState = {
+export const GuildConfigProvider: React.FunctionComponent<
+  GuildConfigProviderProps
+> = (props: GuildConfigProviderProps) => {
+  const web3Context = useWeb3React();
+
+  const [state, setState] = React.useState<GuildServiceProviderState>({
     web3Context: null,
     contract: null,
     address: '',
@@ -45,133 +106,43 @@ class GuildService extends React.Component<
     proposalTime: null,
     timeForExecution: null,
     maxActiveProposals: null,
-  };
+  });
 
-  componentDidMount() {
-    this.initialize(this.props.web3Context, this.props.address);
-  }
-  componentDidUpdate(prevProps: Readonly<GuildServiceProviderProps>) {
-    if (
-      prevProps.address !== this.props.address ||
-      prevProps.web3Context?.chainId !== this.props.web3Context?.chainId
-    ) {
-      this.initialize(this.props.web3Context, this.props.address);
-    }
-  }
+  const location = matchPath<{ address: string }>(window?.location?.hash, {
+    path: '#/guilds/:chain_name/:address',
+  });
+  const address = location?.params?.address;
 
-  initialize = async (
-    web3Context: Web3ReactContextInterface,
-    address: string
-  ) => {
-    console.debug('[GuildServiceContext - initialize]', web3Context, address);
-    const contract = this._getContract(web3Context, address);
-    const config = await this._getGuildConfig(contract);
-
-    this.setState({
-      contract,
-      web3Context,
-      address,
-      token: config.token,
-      permissionRegistry: config.permissionRegistry,
-      name: config.name,
-      proposalTime: config.proposalTime,
-      timeForExecution: config.timeForExecution,
-      maxActiveProposals: config.maxActiveProposals,
-    });
-  };
-
-  private _getContract = (
-    web3Context: Web3ReactContextInterface,
-    address: string
-  ): ERC20Guild | null => {
-    try {
-      const currentProvider = web3Context?.library?.currentProvider;
-      if (!currentProvider) {
+  React.useEffect(() => {
+    const initialize = async () => {
+      console.debug('[GuildServiceContext - initialize]', web3Context, address);
+      if (!address) {
         console.debug(
-          '[GuildServiceContext - _getContract]: currentProvider null'
+          '[GuildServiceContext - initialize] - Cannot find address'
         );
-        return null;
+        return;
       }
-      const provider = new providers.Web3Provider(currentProvider);
-      const contract = getContract(
+      const contract = getContract(web3Context, address);
+      const config = await getGuildConfig(contract);
+
+      setState({
+        contract,
+        web3Context,
         address,
-        ERC20Guild_ABI,
-        provider
-      ) as ERC20Guild;
-      return contract;
-    } catch (e) {
-      console.error(
-        '[GuildServiceContext - _getContract]: Error getting contract instance',
-        e
-      );
-      return null;
-    }
-  };
-  private _getGuildConfig = async (contract: ERC20Guild) => {
-    if (!contract) {
-      console.debug(
-        '[GuildServiceContext - updateGuildConfig]: No contract found'
-      );
-      return {};
-    }
-    try {
-      const [
-        token,
-        permissionRegistry,
-        name,
-        proposalTime,
-        timeForExecution,
-        maxActiveProposals,
-      ] = await Promise.all([
-        contract.getToken(),
-        contract.getPermissionRegistry(),
-        contract.getName(),
-        contract.getProposalTime(),
-        contract.getTimeForExecution(),
-        contract.getMaxActiveProposals(),
-      ]);
-      return {
-        token,
-        permissionRegistry,
-        name,
-        proposalTime,
-        timeForExecution,
-        maxActiveProposals,
-      };
-    } catch (e) {
-      console.error('[GuildProviderService - updateGuildConfig]', e);
-      return {};
-    }
-  };
+        token: config?.token,
+        permissionRegistry: config.permissionRegistry,
+        name: config.name,
+        proposalTime: config.proposalTime,
+        timeForExecution: config.timeForExecution,
+        maxActiveProposals: config.maxActiveProposals,
+      });
+    };
+    initialize();
+  }, [web3Context.chainId, address]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  render() {
-    return (
-      <GuildConfigContext.Provider
-        value={{
-          ...this.state,
-          initialize: this.initialize,
-        }}
-      >
-        {this.props.children}
-      </GuildConfigContext.Provider>
-    );
-  }
-}
-
-const withWeb3ReactAndAddress =
-  (Component: React.ComponentType<GuildServiceProviderProps>) =>
-  (props: { children: React.ReactNode }) => {
-    const web3React = useWeb3React();
-    const location = matchPath<{ address: string }>(window?.location?.hash, {
-      path: '#/guilds/:chain_name/:address',
-    });
-    return (
-      <Component
-        {...props}
-        web3Context={web3React}
-        address={location?.params?.address}
-      />
-    );
-  };
-
-export const GuildConfigProvider = withWeb3ReactAndAddress(GuildService);
+  return (
+    <GuildConfigContext.Provider value={state}>
+      {props.children}
+    </GuildConfigContext.Provider>
+  );
+};
