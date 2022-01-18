cc38b411ca5045115cca8ab574a81328bfc28d30
Milton <milton.tulli@gmail.com>
2022-01-18 18:41:59 -0300

    Set refresh interval to 0

--- src/hooks/Guilds/useConfig.ts
+++ src/hooks/Guilds/useConfig.ts
@@ -16,7 +16,7 @@ export const useConfig = () => {
       [guildAddress, 'getMaxActiveProposals'], // Get the maxActiveProposals
     ],
     {
-      // refreshInterval: 0,
+      refreshInterval: 0,
       initialData: emptyData,
     }
   );
