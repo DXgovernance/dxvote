8661cbc160cdf98e15f4707b53a3ec424fa9f98c
luchux <lucianoguasco@gmail.com>
2022-01-18 20:48:13 -0300

    fix avatar typescript issue and add avatar testing case example

--- src/components/Guilds/Avatar/Avatar.test.tsx
+++ src/components/Guilds/Avatar/Avatar.test.tsx
@@ -1,16 +1,16 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { default as Avatar } from './';
 import { themedComponent } from '../../../utils/tests';
 
-test('renders Avatar with image', async () => {
-  const { container, getByTestId } = render(
-    themedComponent(
-      <Avatar src="algo" defaultSeed={'hola'}>
-        hola
-      </Avatar>
-    )
-  );
-  expect(getByTestId('avatarSrc')).toBeTruthy();
+test('renders Avatar with src', async () => {
+  render(themedComponent(<Avatar src="algo" defaultSeed={'hola'} />));
+  expect(screen.queryByTestId('avatarSrc')).toBeTruthy();
+  expect(screen.queryByTestId('avatarNoSrc')).toBeNull();
+});
+
+test('renders Avatar without src', async () => {
+  render(themedComponent(<Avatar defaultSeed={'hola'} />));
+  expect(screen.queryByTestId('avatarSrc')).toBeNull();
 });
--- src/components/Guilds/Avatar/index.tsx
+++ src/components/Guilds/Avatar/index.tsx
@@ -18,11 +18,7 @@ const Avatar: React.FC<AvatarProps> = ({ src, defaultSeed, size = 24 }) => {
   return src ? (
     <AvatarIcon data-testid="avatarSrc" src={src} alt={'Avatar'} size={size} />
   ) : (
-    <Jazzicon
-      data-testid="avatarNoSrc"
-      diameter={size}
-      seed={jsNumberForAddress(defaultSeed)}
-    />
+    <Jazzicon diameter={size} seed={jsNumberForAddress(defaultSeed)} />
   );
 };
 
