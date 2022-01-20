b0ad7185b5b2a44922af7e2672503f6e54a08636
Hamza <hamza.h.hamud@gmail.com>
2022-01-20 13:51:21 +0000

    fix: place container text into layout folder

---
+++ src/components/Guilds/common/Layout/Text.tsx
@@ -0,0 +1,39 @@
+
+import styled, { css } from 'styled-components';
+import { Flex } from '.';
+
+type ContainerTextProps = {
+    variant?: 'regular' | 'medium' | 'bold';
+};
+
+const variantStyles = (variant = 'regular') =>
+({
+    regular: css`
+      font-weight: 500;
+      font-size: 12px;
+      line-height: 16px;
+    `,
+    medium: css`
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 20px;
+    `,
+
+    bold: css`
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 24px;
+    `,
+}[variant]);
+
+export const ContainerText = styled(Flex) <ContainerTextProps>`
+  font-family: Inter;
+  margin: 4px;
+  font-style: normal;
+  color: ${props => props.color || '#000000'};
+  ${({ variant }) => variantStyles(variant)}
+`;
+
+ContainerText.defaultProps = {
+    variant: 'regular',
+};
