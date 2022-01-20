d28524c564355d02719b1c7e5b06910e37e4ead0
Hamza <hamza.h.hamud@gmail.com>
2022-01-20 14:38:31 +0000

    fix: remove container text from transaction modal

--- src/components/Guilds/Web3Modals/TransactionModal.tsx
+++ src/components/Guilds/Web3Modals/TransactionModal.tsx
@@ -24,42 +24,6 @@ export const ModalButton = styled(Button)`
   }
 `;
 
-type ContainerTextProps = {
-  variant?: 'regular' | 'medium' | 'bold';
-};
-
-const variantStyles = (variant = 'regular') =>
-({
-  regular: css`
-      font-weight: 500;
-      font-size: 12px;
-      line-height: 16px;
-    `,
-  medium: css`
-      font-weight: 500;
-      font-size: 14px;
-      line-height: 20px;
-    `,
-
-  bold: css`
-      font-weight: 600;
-      font-size: 16px;
-      line-height: 24px;
-    `,
-}[variant]);
-
-export const ContainerText = styled(Flex) <ContainerTextProps>`
-  font-family: Inter;
-  margin: 4px;
-  font-style: normal;
-  color: ${props => props.color || '#000000'};
-  ${({ variant }) => variantStyles(variant)}
-`;
-
-ContainerText.defaultProps = {
-  variant: 'regular',
-};
-
 export const Container = styled.div`
   margin: 8px 0 24px 0;
 `;
