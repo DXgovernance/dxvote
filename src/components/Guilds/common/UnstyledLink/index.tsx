fe2a967d315d773ceaea93fe8c0d37906844d0d9
luchux <lucianoguasco@gmail.com>
2022-01-20 14:52:39 -0300

    bring back Link

--- src/components/Guilds/common/UnstyledLink/index.tsx
+++ src/components/Guilds/common/UnstyledLink/index.tsx
@@ -1,7 +1,7 @@
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const UnstyledLink = styled.a`
+const UnstyledLink = styled(Link)`
   text-decoration: inherit;
   color: inherit;
 `;
