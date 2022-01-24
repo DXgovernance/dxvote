2c0ba0f59bbace4a2422bd5f3b4f26bbe634da98
rossneilson <rossneilson99@gmail.com>
2022-01-24 14:18:13 +0000

    feat: Removing css file

--- src/components/Guilds/Editor/index.tsx
+++ src/components/Guilds/Editor/index.tsx
@@ -1,11 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Focus from '@tiptap/extension-focus';
 import Highlight from '@tiptap/extension-highlight';
 import MenuBar from './MenuBar';
 import TurndownService from 'turndown';
-import './styles.scss';
 
 var turndownService = new TurndownService();
 
@@ -26,6 +25,95 @@ const Content = styled(EditorContent)`
   overflow-y: auto;
   padding: 0 1rem;
   -webkit-overflow-scrolling: touch;
+  ${({}) =>
+    css`
+      .ProseMirror {
+        > * + * {
+          margin-top: 0.75em;
+        }
+
+        outline: none;
+        min-height: 200px;
+
+        ul,
+        ol {
+          padding: 0 1rem;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          line-height: 1.1;
+        }
+
+        code {
+          background-color: rgba(#616161, 0.1);
+          color: #616161;
+        }
+
+        pre {
+          background: #0d0d0d;
+          border-radius: 10px;
+          color: #fff;
+          font-family: 'JetBrainsMono', monospace;
+          padding: 0.75rem 1rem;
+
+          code {
+            background: none;
+            color: inherit;
+            font-size: 0.8rem;
+            padding: 0;
+          }
+        }
+
+        mark {
+          background-color: #faf594;
+        }
+
+        img {
+          height: auto;
+          max-width: 100%;
+        }
+
+        hr {
+          margin: 1rem 0;
+        }
+
+        blockquote {
+          border-left: 2px solid rgba(#0d0d0d, 0.1);
+          padding-left: 1rem;
+        }
+
+        hr {
+          border: none;
+          border-top: 2px solid rgba(#0d0d0d, 0.1);
+          margin: 2rem 0;
+        }
+
+        ul[data-type='taskList'] {
+          list-style: none;
+          padding: 0;
+
+          li {
+            align-items: center;
+            display: flex;
+
+            > label {
+              flex: 0 0 auto;
+              margin-right: 0.5rem;
+              user-select: none;
+            }
+
+            > div {
+              flex: 1 1 auto;
+            }
+          }
+        }
+      }
+    `}
 `;
 
 const Editor = () => {
