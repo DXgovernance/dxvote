220e83b72eb99885c87c7b3989d31130432c98aa
rossneilson <rossneilson99@gmail.com>
2022-01-24 14:21:26 +0000

    style: Changing empty function to js wrappper only

--- src/components/Guilds/Editor/index.tsx
+++ src/components/Guilds/Editor/index.tsx
@@ -25,95 +25,94 @@ const Content = styled(EditorContent)`
   overflow-y: auto;
   padding: 0 1rem;
   -webkit-overflow-scrolling: touch;
-  ${({}) =>
-    css`
-      .ProseMirror {
-        > * + * {
-          margin-top: 0.75em;
-        }
+  ${css`
+    .ProseMirror {
+      > * + * {
+        margin-top: 0.75em;
+      }
 
-        outline: none;
-        min-height: 200px;
+      outline: none;
+      min-height: 200px;
 
-        ul,
-        ol {
-          padding: 0 1rem;
-        }
+      ul,
+      ol {
+        padding: 0 1rem;
+      }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          line-height: 1.1;
-        }
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        line-height: 1.1;
+      }
+
+      code {
+        background-color: rgba(#616161, 0.1);
+        color: #616161;
+      }
+
+      pre {
+        background: #0d0d0d;
+        border-radius: 10px;
+        color: #fff;
+        font-family: 'JetBrainsMono', monospace;
+        padding: 0.75rem 1rem;
 
         code {
-          background-color: rgba(#616161, 0.1);
-          color: #616161;
-        }
-
-        pre {
-          background: #0d0d0d;
-          border-radius: 10px;
-          color: #fff;
-          font-family: 'JetBrainsMono', monospace;
-          padding: 0.75rem 1rem;
-
-          code {
-            background: none;
-            color: inherit;
-            font-size: 0.8rem;
-            padding: 0;
-          }
-        }
-
-        mark {
-          background-color: #faf594;
-        }
-
-        img {
-          height: auto;
-          max-width: 100%;
-        }
-
-        hr {
-          margin: 1rem 0;
-        }
-
-        blockquote {
-          border-left: 2px solid rgba(#0d0d0d, 0.1);
-          padding-left: 1rem;
-        }
-
-        hr {
-          border: none;
-          border-top: 2px solid rgba(#0d0d0d, 0.1);
-          margin: 2rem 0;
-        }
-
-        ul[data-type='taskList'] {
-          list-style: none;
+          background: none;
+          color: inherit;
+          font-size: 0.8rem;
           padding: 0;
+        }
+      }
 
-          li {
-            align-items: center;
-            display: flex;
+      mark {
+        background-color: #faf594;
+      }
 
-            > label {
-              flex: 0 0 auto;
-              margin-right: 0.5rem;
-              user-select: none;
-            }
+      img {
+        height: auto;
+        max-width: 100%;
+      }
 
-            > div {
-              flex: 1 1 auto;
-            }
+      hr {
+        margin: 1rem 0;
+      }
+
+      blockquote {
+        border-left: 2px solid rgba(#0d0d0d, 0.1);
+        padding-left: 1rem;
+      }
+
+      hr {
+        border: none;
+        border-top: 2px solid rgba(#0d0d0d, 0.1);
+        margin: 2rem 0;
+      }
+
+      ul[data-type='taskList'] {
+        list-style: none;
+        padding: 0;
+
+        li {
+          align-items: center;
+          display: flex;
+
+          > label {
+            flex: 0 0 auto;
+            margin-right: 0.5rem;
+            user-select: none;
+          }
+
+          > div {
+            flex: 1 1 auto;
           }
         }
       }
-    `}
+    }
+  `}
 `;
 
 const Editor = () => {
