7a0586c90d86079a3b577a053ccad380d67f8b1c
luchux <lucianoguasco@gmail.com>
2022-01-24 17:28:33 -0300

    fixing bug in status undefined

--- src/components/Guilds/ProposalStatus/index.tsx
+++ src/components/Guilds/ProposalStatus/index.tsx
@@ -49,7 +49,6 @@ interface ProposalStatusProps {
   //optional cause
   //if not present can { guild_id, proposal_id } = useParams()
   proposalId?: string;
-  // proposal: Proposal;
   bordered?: boolean;
   hideTime?: boolean;
   showRemainingTime?: boolean;
@@ -58,17 +57,19 @@ interface ProposalStatusProps {
 const ProposalStatus: React.FC<ProposalStatusProps> = ({
   proposalId,
   bordered,
-  // proposal,
   hideTime,
   showRemainingTime,
 }) => {
-  const { guild_id, proposal_id } = useParams<{
+  const { guild_id, proposal_id: paramProposalId } = useParams<{
     guild_id?: string;
     proposal_id?: string;
   }>();
 
   // we need to type useProposal
-  const { proposal }: any = useProposal(guild_id, proposalId || proposal_id);
+  const { data: proposal, isValidating }: any = useProposal(
+    guild_id,
+    proposalId || paramProposalId
+  );
 
   const endTime = useMemo(() => {
     if (!proposal) return null;
@@ -87,7 +88,7 @@ const ProposalStatus: React.FC<ProposalStatusProps> = ({
   }, [endTime, showRemainingTime, hideTime]);
 
   const statusDetail = useMemo(() => {
-    if (!proposal || !endTime) return null;
+    if (!proposal || !endTime || isValidating) return null;
 
     if (proposal.state === ProposalState.Submitted) {
       const currentTime = moment();
@@ -100,7 +101,7 @@ const ProposalStatus: React.FC<ProposalStatusProps> = ({
     return 'Ended';
 
     // return proposal.state;
-  }, [endTime, proposal]);
+  }, [endTime, proposal, isValidating]);
 
   return (
     <Status test-id="proposal-status" bordered={hideTime ? false : bordered}>
@@ -116,14 +117,7 @@ const ProposalStatus: React.FC<ProposalStatusProps> = ({
         </DetailText>
       )}
       <Pill filled padded>
-        {statusDetail || (
-          <Skeleton
-            test-id="skeleton"
-            width={50}
-            baseColor="#333"
-            highlightColor="#555"
-          />
-        )}
+        {statusDetail || <Skeleton test-id="skeleton-pill" width={50} />}
       </Pill>
     </Status>
   );
