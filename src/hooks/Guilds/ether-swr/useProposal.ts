5b7703403bac34068b2823f1e71624b9ff3ea484
Madusha Prasanjith <hi@madusha.me>
2022-01-25 20:33:21 +0530

    feat: (try to) update the proposal with swr middleware.

--- src/hooks/Guilds/ether-swr/useProposal.ts
+++ src/hooks/Guilds/ether-swr/useProposal.ts
@@ -1,10 +1,14 @@
 import useEtherSWR from 'ether-swr';
+import { unix } from 'moment';
 import { Middleware, SWRHook } from 'swr';
 import { Proposal } from '../../../types/types.guilds';
 
 const formatterMiddleware: Middleware =
   (useSWRNext: SWRHook) => (key, fetcher, config) => {
     const swr = useSWRNext(key, fetcher, config);
+    if (swr.data) {
+      swr.data['startTime'] = unix(swr.data['startTime'].toNumber());
+    }
     return swr;
   };
 
