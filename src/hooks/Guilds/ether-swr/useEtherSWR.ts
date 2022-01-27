de307f7d653ba38754d3a0ac75fdcd2bcadc224c
Madusha Prasanjith <hi@madusha.me>
2022-01-27 23:55:24 +0530

    feat: Add multichain-capable version of the ethers-swr hook.

---
+++ src/hooks/Guilds/ether-swr/useEtherSWR.ts
@@ -0,0 +1,42 @@
+import { useWeb3React } from '@web3-react/core';
+import useEtherSWRHook, {
+  etherKeyFuncInterface,
+  ethKeyInterface,
+  ethKeysInterface,
+  EthSWRConfigInterface,
+} from 'ether-swr';
+import { useEffect } from 'react';
+import { SWRResponse } from 'swr';
+import { usePrevious } from '../usePrevious';
+
+function useEtherSWR<Data = any, Error = any>(
+  key: ethKeyInterface | ethKeysInterface | etherKeyFuncInterface
+): SWRResponse<Data, Error>;
+function useEtherSWR<Data = any, Error = any>(
+  key: ethKeyInterface | ethKeysInterface | etherKeyFuncInterface,
+  config?: EthSWRConfigInterface<Data, Error>
+): SWRResponse<Data, Error>;
+function useEtherSWR<Data = any, Error = any>(
+  key: ethKeyInterface | ethKeysInterface | etherKeyFuncInterface,
+  fetcher?: any,
+  config?: EthSWRConfigInterface<Data, Error>
+): SWRResponse<Data, Error>;
+function useEtherSWR<Data = any, Error = any>(
+  ...args: any[]
+): SWRResponse<Data, Error> {
+  // @ts-ignore - TS gets confused here about the number of args
+  const swrResponse = useEtherSWRHook<Data, Error>(...args);
+  const { chainId } = useWeb3React();
+
+  const prevChainId = usePrevious(chainId);
+  useEffect(() => {
+    if (prevChainId && chainId !== prevChainId) {
+      console.log('Different chain ID. Mutating...');
+      swrResponse.mutate(null);
+    }
+  }, [chainId, prevChainId, swrResponse]);
+
+  return swrResponse;
+}
+
+export default useEtherSWR;
