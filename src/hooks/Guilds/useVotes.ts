9150b72f086d29e5d49d375195931f4460fe9519
Hamza <hamza.h.hamud@gmail.com>
2022-02-02 17:34:09 +0000

    new: useVote guilds hook

---
+++ src/hooks/Guilds/useVotes.ts
@@ -0,0 +1,37 @@
+import { useCallback } from 'react';
+import { BigNumber, ContractTransaction } from 'ethers';
+import { useParams } from 'react-router-dom';
+import { useERC20Guild } from './contracts/useContract';
+
+//@TODO convert actions into set votes parameter
+// transaction Modal
+// @TODO actions?
+// @TODO votingPower calculations
+
+interface useVotesReturns {
+    setVote: (
+        action: BigNumber,
+        votingPower: BigNumber
+    ) => Promise<ContractTransaction>;
+    //  setVotes: (action: BigNumber, votingPower: BigNumber) => Promise<ContractTransaction>;
+    //    setSignedVote: () => void;
+    //   setSignedVotes: () => void;
+}
+
+export const useVotes = (): useVotesReturns => {
+    const { guild_id: guildId, proposal_id: proposalId } =
+        useParams<{ guild_id?: string; proposal_id?: string }>();
+
+    const contract = useERC20Guild(guildId, true);
+
+    const setVote = useCallback(
+        (action: BigNumber, votingPower: BigNumber) => {
+            return contract.setVote(proposalId, action, votingPower);
+        },
+        [contract, guildId, proposalId]
+    );
+
+    return {
+        setVote,
+    };
+};
