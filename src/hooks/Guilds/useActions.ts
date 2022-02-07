b12b8ab311d50c1b173cfb2b7cd2d3e560819c0f
Hamza <hamza.h.hamud@gmail.com>
2022-02-07 16:27:28 +0000

    fix: remove actions has this will be fetched offchain

--- src/hooks/Guilds/useActions.ts
+++ src/hooks/Guilds/useActions.ts
@@ -3,7 +3,6 @@ import { useProposal } from './ether-swr/useProposal';
 import { useMemo } from 'react';
 
 interface useActionsReturns {
-  actions: string[];
   possibleActions: number[];
 }
 
@@ -20,24 +19,11 @@ export const useActions = (): useActionsReturns => {
   // the smart contract limits the contract calls that action can make to those
   // recorded on the permission registry contract
   // just decode each call according to those in the function signatures in the recorded array
-  // @TODO reset active proposals limit and create new proposals
   const possibleActions = useMemo(() => {
-    return [...Array(proposal.totalActions.toNumber()).keys()];
+    return [...Array(proposal.totalActions.toNumber() + 1).keys()];
   }, [proposal]);
 
-  const actions = useMemo(() => {
-    return proposal.data.map((item, i) => {
-      if (i == 0) {
-        return 'No';
-      }
-      console.log(item);
-      //@TODO decode action call data and extract function name, e.g. yes APPROVE/TRANSFER
-      return 'yes';
-    });
-  }, [guildId, proposalId]);
-
   return {
-    actions,
     possibleActions,
   };
 };
