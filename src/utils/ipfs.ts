ff33247402c9d27624e9a8a8945f9b537e34d3a7
AugustoL <augustolemble@pm.me>
2022-02-23 17:43:02 -0700

    chore(src): format code

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -5,33 +5,38 @@ export const getIPFSFile = async function (
   ipfsHash,
   timeout = 1000
 ): Promise<AxiosResponse<any>> {
-  return await Promise.any([
-    axios.request({
-      url: 'https://ipfs.io/ipfs/' + ipfsHash,
-      method: 'GET',
-      timeout: timeout,
-    }),
-    axios.request({
-      url: 'https://gateway.ipfs.io/ipfs/' + ipfsHash,
-      method: 'GET',
-      timeout: timeout,
-    }),
-    axios.request({
-      url: 'https://gateway.pinata.cloud/ipfs/' + ipfsHash,
-      method: 'GET',
-      timeout: timeout,
-    }),
-    axios.request({
-      url: 'https://dweb.link/ipfs/' + ipfsHash,
-      method: 'GET',
-      timeout: timeout,
-    }),
-    axios.request({
-      url: 'https://infura-ipfs.io/ipfs/' + ipfsHash,
-      method: 'GET',
-      timeout: timeout,
-    }),
-  ]);
+  try {
+    return await Promise.any([
+      axios.request({
+        url: 'https://ipfs.io/ipfs/' + ipfsHash,
+        method: 'GET',
+        timeout: timeout,
+      }),
+      axios.request({
+        url: 'https://gateway.ipfs.io/ipfs/' + ipfsHash,
+        method: 'GET',
+        timeout: timeout,
+      }),
+      axios.request({
+        url: 'https://gateway.pinata.cloud/ipfs/' + ipfsHash,
+        method: 'GET',
+        timeout: timeout,
+      }),
+      axios.request({
+        url: 'https://dweb.link/ipfs/' + ipfsHash,
+        method: 'GET',
+        timeout: timeout,
+      }),
+      axios.request({
+        url: 'https://infura-ipfs.io/ipfs/' + ipfsHash,
+        method: 'GET',
+        timeout: timeout,
+      }),
+    ]);
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
 };
 
 export const descriptionHashToIPFSHash = function (descriptionHash) {
