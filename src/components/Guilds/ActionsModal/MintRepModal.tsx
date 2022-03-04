ec1774409f654a8ba822bffa73d53caaf5838ef7
Hamza <hamza.h.hamud@gmail.com>
2022-03-04 18:51:09 +0000

    move mintRep modal to its own file

---
+++ src/components/Guilds/ActionsModal/MintRepModal.tsx
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+import { Input } from '../../../components/Guilds/common/Form';
+import { useWeb3React } from "@web3-react/core";
+import { useState } from "react";
+
+
+//icons
+import arbitrumIcon from '../../../assets/images/arbitrum.png';
+import ethereumIcon from '../../../assets/images/ethereum.svg';
+import gnosisIcon from '../../../assets/images/gnosis-icon-green.svg';
+import { Flex } from '../common/Layout';
+import { Modal } from '../common/Modal';
+import { ContainerText } from '../common/Layout/Text';
+
+
+const RepWrapper = styled(Flex)`
+margin: 16px auto;
+`
+const WrapperText = styled(ContainerText)`
+  justify-content: left;
+  flex-direction: row;
+  width: 85%;
+margin: 8px auto;
+`;
+
+
+const ExternalWrapper = styled(Flex)`
+  width: 100%;
+margin: 8px auto;
+`;
+
+interface MintReputationModalProps {
+    isOpen: boolean,
+    onCancel: () => void
+}
+
+const MintReputationModal: React.FC<MintReputationModalProps> = ({ isOpen, onCancel }) => {
+    const [address, setAddress] = useState('')
+    const [repAmount, setRepAmount] = useState('')
+    const [repPercent, setRepPercent] = useState('')
+    const { chainId } = useWeb3React();
+
+
+    const iconsByChain = {
+        1: ethereumIcon,
+        4: ethereumIcon,
+        100: gnosisIcon,
+        42161: arbitrumIcon,
+        421611: arbitrumIcon,
+        1337: ethereumIcon,
+    };
+
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onDismiss={onCancel}
+            header={<div>Mint Reputation</div>}
+            maxWidth={400}
+            confirmText="Add Action"
+            onConfirm={() => console.log('test action button')}
+        >
+            <RepWrapper>
+                <ExternalWrapper>
+                    <WrapperText variant="bold" color="grey">
+                        Recipient
+                    </WrapperText>
+                    <Input
+                        placeholder="Ethereum Address"
+                        value={address}
+                        onChange={e => setAddress(e.target.value)}
+                        icon={iconsByChain[chainId] || null}
+                        size={24}
+                        width='85%'
+                        cross
+                    />
+                </ExternalWrapper>
+                <ExternalWrapper>
+                    <WrapperText variant="bold" color="grey">
+                        Reputation Amount
+                    </WrapperText>
+                    <Input
+                        placeholder="0.00"
+                        value={repAmount}
+                        onChange={e => setRepAmount(e.target.value)}
+                        size={24}
+                        width='85%'
+                        cross
+                    />
+                </ExternalWrapper>
+                <ExternalWrapper>
+                    <WrapperText variant="bold" color="grey">
+                        Reputation in %
+                    </WrapperText>
+                    <Input
+                        placeholder="0.00"
+                        value={repPercent}
+                        onChange={e => setRepPercent(e.target.value)}
+                        width="85%"
+                        cross
+                    />
+                </ExternalWrapper>
+            </RepWrapper>
+        </Modal>
+
+
+
+    )
+}
+
+export default MintReputationModal;
--- src/components/Guilds/ActionsModal/index.tsx
+++ src/components/Guilds/ActionsModal/index.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from '../common/Modal';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { Flex } from '../common/Layout';
 import { ContainerText } from '../common/Layout/Text';
 import { Button } from '../common/Button';
 import { ReactComponent as Vector } from '../../../assets/images/vector.svg';
 import StyledIcon from '../common/SVG';
+import MintReputationModal from './MintRepModal';
+
+
 
 interface ActionModalProps {
-    isOpen: boolean
-    onCancel: () => void
+    isOpen: boolean;
+    onCancel: () => void;
 }
 
 const CoreWrapper = styled(Flex)`
-width: 100%;
-margin-bottom: 16px;
-`
-
+  width: 100%;
+  margin-bottom: 16px;
+`;
 
 const ExternalWrapper = styled(Flex)`
-width: 100%;
-`
+  width: 100%;
+`;
 
 const Wrapper = styled(Flex)`
-width: 100%;
-margin: 24px auto;
-`
+  width: 100%;
+  margin: 24px auto;
+`;
 
 const ActionsButton = styled(Button)`
   width: 90%;
@@ -40,23 +42,31 @@ const ActionsButton = styled(Button)`
 `;
 
 const WrapperText = styled(ContainerText)`
-justify-content: left;
-flex-direction: row;
-width: 85%
-`
+  justify-content: left;
+  flex-direction: row;
+  width: 85%;
+`;
+
 const ButtonText = styled(ContainerText)`
-justify-content: space-between;
-flex-direction: row;
-`
+  justify-content: space-between;
+  flex-direction: row;
+`;
 
 const ExternalButton = styled(ActionsButton)`
   justify-content: space-between;
-`
+`;
+
+
 
 const ActionModal: React.FC<ActionModalProps> = ({ isOpen, onCancel }) => {
-    //TODO: check if transfer and mint has already been selected --> disable
-    //TODO: create mint reputation modal
-    //TODO: transfer funds modal
+    //TODO: remove hardcoded external contracts with actual data
+
+
+    const [mintRep, setMintRep] = useState(false)
+
+    const handleClose = () => setMintRep(false)
+
+
     return (
         <Modal
             isOpen={isOpen}
@@ -65,67 +75,66 @@ const ActionModal: React.FC<ActionModalProps> = ({ isOpen, onCancel }) => {
             maxWidth={300}
         >
             <Wrapper>
+                <MintReputationModal isOpen={mintRep} onCancel={handleClose} />
                 <CoreWrapper>
-                    <WrapperText variant="bold" color='grey'>
+                    <WrapperText variant="bold" color="grey">
                         Core
                     </WrapperText>
-                    <ActionsButton variant='secondary'>
+                    <ActionsButton variant="secondary" onClick={() => setMintRep(true)}>
                         <StyledIcon src={Vector} />
                         Transfer & Mint
                     </ActionsButton>
                 </CoreWrapper>
                 <ExternalWrapper>
-                    <WrapperText variant="bold" color='grey'>
+                    <WrapperText variant="bold" color="grey">
                         External Contracts
                     </WrapperText>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         DXdao Controller
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             2 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         Permissions Registry
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             4 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         DXD Voting Machine
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             1 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         RegistrarWalletScheme
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             2 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         MasterWalletScheme
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             1 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         QuickWalletScheme
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             1 Actions
                         </ButtonText>
                     </ExternalButton>
-                    <ExternalButton variant='secondary'>
+                    <ExternalButton variant="secondary">
                         SWPRWalletScheme
-                        <ButtonText variant="medium" color='grey'>
+                        <ButtonText variant="medium" color="grey">
                             1 Actions
                         </ButtonText>
                     </ExternalButton>
                 </ExternalWrapper>
             </Wrapper>
         </Modal>
-
-    )
-}
-
+    );
+};
 
 export default ActionModal;
