5b787c326e35c62bd1df83e9df6da10e61bf501b
Hamza <hamza.h.hamud@gmail.com>
2022-03-10 22:21:19 +0000

    fix: temp file

--- src/components/Guilds/CreateProposalPage/ActionsBuilder/TransfersAndMint.tsx
+++ src/components/Guilds/CreateProposalPage/ActionsBuilder/TransfersAndMint.tsx
@@ -1,1 +1,7 @@
-// create transfer and mints component
+// create transfer and mints componen
+
+const TransferAndMint: React.FC = () => {
+  return <div>hi</div>;
+};
+
+export default TransferAndMint;
