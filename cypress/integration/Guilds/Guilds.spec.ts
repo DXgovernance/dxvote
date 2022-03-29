f256f0742071911d5f98927c99d9a6342bd24670
Milton <milton.tulli@gmail.com>
2022-03-29 16:56:05 -0300

    Fix integration test

--- cypress/integration/Guilds/Guilds.spec.ts
+++ cypress/integration/Guilds/Guilds.spec.ts
@@ -9,7 +9,7 @@ describe('Guilds', () => {
 
   it('Should render proper components on Guild page', () => {
     const guildAddress = Guilds.deployedGuildsAddresses[0];
-    expect(Guilds.deployedGuildsAddresses.length).to.be.equal(2);
+    expect(Guilds.deployedGuildsAddresses.length).to.be.equal(3);
     Guilds.goToGuildsPage('localhost', guildAddress);
     Guilds.shouldRenderProposalsList();
     Guilds.shouldRenderSidebar();
