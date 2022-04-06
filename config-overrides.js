8db679b81ed8470afecfb5117796b7a8997d83e3
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-04-06 12:25:33 +0200

    Upgrade to Webpack5 #769

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,26 @@
 // Used to make the build reproducible between different machines (IPFS-related)
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = (config, env) => {
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: { fullySpecified: false },
+  });
+
+  // Fix import outside src issue
+  config.resolve.plugins = config.resolve.plugins.filter(
+    plugin => plugin.constructor.name !== `ModuleScopePlugin`
+  );
+
+  // Add polyfills to crypto, stream, path etc...
+  config.plugins.push(new NodePolyfillPlugin());
+
   if (env !== 'production') {
     return config;
   }
+
   config.output.filename = `static/js/[name].js`;
   config.output.chunkFilename = `static/js/[name].chunk.js`;
   return config;
 };
+
