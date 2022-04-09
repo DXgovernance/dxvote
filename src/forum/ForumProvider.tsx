68e55125e002a1be5f16c1380dafe700da177228
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-04-09 09:34:43 +0200

    Update @self.id/react to use @next

--- src/forum/ForumProvider.tsx
+++ src/forum/ForumProvider.tsx
@@ -4,8 +4,6 @@ import type { ModelTypeAliases, ModelTypesToAliases } from '@glazed/types';
 import publishedModel from './model.json';
 
 import { Provider } from '@self.id/react';
-import Ceramic from '@self.id/react';
-console.log(Ceramic);
 
 const model: ModelTypesToAliases<ModelTypeAliases<{}, {}>> = publishedModel;
 
