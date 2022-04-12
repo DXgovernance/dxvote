0794bb97b7b2a1b2b3d13bc21da7ae362db4fbb1
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-04-12 09:52:06 +0200

    Add OrbitDB to ForumProvider #769

--- src/forum/ForumProvider.tsx
+++ src/forum/ForumProvider.tsx
@@ -1,9 +1,11 @@
+import React, { createContext, useEffect, useState } from 'react';
 import { ConnectNetwork } from '@self.id/web';
 import type { ModelTypeAliases, ModelTypesToAliases } from '@glazed/types';
+import { Provider } from '@self.id/react';
+
+import { useContext } from 'contexts';
 
 import publishedModel from './model.json';
-
-import { Provider } from '@self.id/react';
 
 const model: ModelTypesToAliases<ModelTypeAliases<{}, {}>> = publishedModel;
 
@@ -13,8 +15,47 @@ const client = {
   model,
 };
 
+const OrbitDB = createContext({});
+
+export const useForum = () => React.useContext(OrbitDB);
+
+function useOrbitDB() {
+  const { context } = useContext();
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    if (context.orbitDBService) {
+      context.orbitDBService.getOrbitDB().then(async orbitdb => {
+        // Do we want to use a seperate DB for each chain?
+        const dbName = `dxforum-0.1_${process.env.NODE_ENV || 'development'}`;
+        const dbConfig = {
+          create: true,
+          sync: false,
+          type: 'docstore',
+          indexBy: 'id',
+          accessController: {
+            write: ['*'],
+          },
+        };
+        const registry = await orbitdb.open(dbName, dbConfig);
+
+        registry.load();
+
+        setState({ orbitdb, registry });
+      });
+    }
+  }, [context.orbitDBService]);
+
+  return state;
+}
+
 function ForumProvider({ children }) {
-  return <Provider client={client}>{children}</Provider>;
+  const value = useOrbitDB();
+  return (
+    <Provider client={client}>
+      <OrbitDB.Provider value={value}>{children}</OrbitDB.Provider>
+    </Provider>
+  );
 }
 
 export default ForumProvider;
