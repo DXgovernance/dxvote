3113e9fff9042973d257b27e62f6478960c13182
Milton Tulli <milton.tulli@gmail.com>
2022-04-12 20:32:32 -0300

    Create VoteConfirmationModal (#788)
    * Use env accounts instead of hardcoded ones (#780)
    
    * Create VoteConfirmationModal
    
    * Change Cancel Button style
    
    * refactor styles

---
+++ src/components/Guilds/ProposalPage/ProposalVoteCard/VoteConfirmationModal/VoteConfirmationModal.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  Container,
+  Title,
+  InfoItem,
+  Widget,
+  InfoRow,
+  InfoLabel,
+  InfoValue,
+  ActionWrapper,
+  CancelButton,
+  ConfirmButton,
+} from './VoteConfirmationModal.styled';
+import { Modal } from 'components/Guilds/common/Modal';
+
+interface VoteConfirmationModalProps {
+  isOpen: boolean;
+  onDismiss: () => void;
+  selectedAction?: string;
+  onConfirm: () => void;
+  votingPower?: number;
+  totalLocked?: number;
+}
+
+export const VoteConfirmationModal: React.FC<VoteConfirmationModalProps> = ({
+  isOpen,
+  onDismiss,
+  onConfirm,
+  selectedAction,
+  votingPower,
+  totalLocked,
+}) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+      header="Confirm Vote"
+      leftIcon={false}
+      maxWidth={380}
+    >
+      <Container>
+        <Title>Are you sure you want to vote "{selectedAction}"?</Title>
+        <InfoItem>This action cannot be reverted</InfoItem>
+
+        <Widget>
+          <InfoRow>
+            <InfoLabel>Option</InfoLabel>
+            <InfoValue>{selectedAction}</InfoValue>
+          </InfoRow>
+          <InfoRow>
+            <InfoLabel>Voting Power</InfoLabel>
+            <InfoValue>{votingPower}%</InfoValue>
+          </InfoRow>
+          <InfoRow>
+            <InfoLabel>Vote Impact</InfoLabel>
+            <InfoValue>
+              <InfoValue grey> {totalLocked}% </InfoValue>
+              {' -> '} {votingPower}%
+            </InfoValue>
+          </InfoRow>
+        </Widget>
+        <ActionWrapper>
+          <CancelButton onClick={onDismiss}>Cancel</CancelButton>
+          <ConfirmButton onClick={onConfirm}>Vote</ConfirmButton>
+        </ActionWrapper>
+      </Container>
+    </Modal>
+  );
+};
