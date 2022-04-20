2f287d7032341c0abb8f1298e3f174ed574cddb0
rossneilson <rossneilson99@gmail.com>
2022-04-20 16:14:15 +0100

    fix: Remove unused component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -23,26 +23,6 @@ export const Button = styled.button`
     cursor: auto;
   }
 `;
-export const RouterButton = styled(Link)`
-  background-color: ${({ theme }) => theme.activeButtonBackground};
-  border-radius: 4px;
-  color: white;
-  height: 34px;
-  letter-spacing: 1px;
-  font-weight: 500;
-  text-align: center;
-  cursor: pointer;
-  width: ${props => (props.width ? props.width : 'max-content')};
-  padding: 0px 10px;
-  margin: 5px;
-  border: 0px;
-  text-transform: uppercase;
-  letter-spacing: 0.1em;
-  :disabled {
-    opacity: 0.4;
-    cursor: auto;
-  }
-`;
 
 interface LinkButtonProps {
   route: string;
