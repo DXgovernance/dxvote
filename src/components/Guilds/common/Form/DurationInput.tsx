72ee7fdfddc73d58dcad29dd47c6ae599f1224d5
Hamza <hamza.h.hamud@gmail.com>
2022-05-01 16:52:43 +0100

    fix(guilds/DurationInput): convert duration input into total seconds

--- src/components/Guilds/common/Form/DurationInput.tsx
+++ src/components/Guilds/common/Form/DurationInput.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { Modal } from '../Modal';
 import { Flex } from '../Layout/Box';
 import { Button } from '../Button';
+import moment, { Duration, DurationInputArg2 } from 'moment';
 
 const Column = styled(Flex)`
   flex-direction: column;
@@ -16,56 +17,83 @@ interface DurationInputProps {
   onDismiss: () => void;
 }
 
-interface CalenderLimits {
-  years: number[];
-  months: number[];
-  days: number[];
-  hours: number[];
-  minutes: number[];
-  seconds: number[];
-}
-
-interface QuantityProps {
-  constraints: CalenderLimits;
-  constraint: string;
-}
-
-const useCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increment = () => setCounter(prevCount => prevCount + 1);
-  const decrement = () => setCounter(prevCount => prevCount - 1);
-
-  return { counter, setCounter, increment, decrement };
-};
-
-const Quantity: React.FC<QuantityProps> = ({ constraints, constraint }) => {
-  const { counter, increment, decrement } = useCounter();
-
-  return (
-    <Column>
-      <Button disabled={counter >= constraints[constraint][1]} onClick={increment} />
-      <div>{counter}</div>
-      <Button disabled={counter <= constraints[constraint][0]} onClick={decrement} />
-    </Column>
-  );
+export const DURATION_LIMITS = {
+  years: {
+    min: 0,
+    max: 10,
+  },
+  months: {
+    min: 0,
+    max: 11,
+  },
+  days: {
+    min: 0,
+    max: 31,
+  },
+  hours: {
+    min: 0,
+    max: 23,
+  },
+  minutes: {
+    min: 0,
+    max: 59,
+  },
+  seconds: {
+    min: 0,
+    max: 59,
+  },
 };
 
 const DurationInput: React.FC<DurationInputProps> = ({ isOpen, onDismiss }) => {
-  const constraints = {
-    years: [0, 10],
-    months: [0, 11],
-    days: [0, 29],
-    hours: [0, 23],
-    minutes: [0, 59],
-    seconds: [0, 59],
-  };
+  const [duration, setDuration] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const increment = (key: string) =>
+    setDuration({ ...duration, [key]: duration[key] + 1 });
+  const decrement = (key: string) =>
+    setDuration({ ...duration, [key]: duration[key] - 1 });
+
+  const { time } = useMemo(() => {
+    const convertDurationToSeconds = Object.keys(duration).reduce(
+      (acc, curr) => {
+        const result = acc.add(
+          moment.duration(duration[curr], curr as DurationInputArg2)
+        );
+        return result;
+      },
+      moment.duration(0, 'years') as Duration
+    );
+    return {
+      time: convertDurationToSeconds,
+    };
+  }, [duration]);
+
+  console.log('Time: ' + time.asSeconds());
 
   return (
     <Modal isOpen={isOpen} onDismiss={onDismiss}>
       <Container>
-        {Object.keys(constraints).map(constraint => {
-          return <Quantity constraints={constraints} constraint={constraint} />;
+        {Object.keys(duration).map(value => {
+          const count = duration[value];
+          return (
+            <Column>
+              <Button
+                disabled={count >= DURATION_LIMITS[value].max}
+                onClick={() => increment(value)}
+              />
+              <div>{count}</div>
+              <Button
+                disabled={count <= DURATION_LIMITS[value].min}
+                onClick={() => decrement(value)}
+              />
+            </Column>
+          );
         })}
       </Container>
     </Modal>
