bad88430e3c3d33eeebc2c4ab761e8ccc2780c96
Madusha Prasanjith <hi@madusha.me>
2022-05-10 12:27:58 +0530

    feat: Add tests for ENS avatar.

---
+++ src/components/Guilds/Avatar/ENSAvatar.test.tsx
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { ZERO_ADDRESS } from 'utils';
+import { render } from '../../../utils/tests';
+
+import ENSAvatar from './ENSAvatar';
+
+const testImage = '/image/test.jpg';
+
+jest.mock('hooks/Guilds/ether-swr/ens/useENSAvatar', (address?: string) => {
+  return {
+    __esModule: true,
+    default: () => ({
+      imageUrl: address ? testImage : null,
+    }),
+  };
+});
+
+describe('ENSAvatar', () => {
+  it('Should render avatar when address is given', () => {
+    const { container } = render(<ENSAvatar address={ZERO_ADDRESS} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should render skeleton when address is not given', () => {
+    const { container } = render(<ENSAvatar />);
+    expect(container).toMatchSnapshot();
+  });
+});
