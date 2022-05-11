a9ab2f6728f255aefc446abcdb5fca24f2bde98a
Dino <crescimbenidino@gmail.com>
2022-05-11 16:16:18 -0300

    feat: added permissions button and fixed minor bug that prevented proposal creation

--- src/contexts/Guilds/guildAvailability.tsx
+++ src/contexts/Guilds/guildAvailability.tsx
@@ -35,10 +35,10 @@ export const GuildAvailabilityContext =
   createContext<GuildAvailabilityContextInterface>({});
 
 const GuildAvailabilityProvider = ({ children }) => {
-  const routeMatch = useRouteMatch<{ guildId?: string }>(
+  const routeMatch = useRouteMatch<{ guild_id?: string }>(
     '/:chain_name/:guild_id'
   );
-  const guildId = routeMatch?.params?.guildId;
+  const guildId = routeMatch?.params?.guild_id;
   const { providers: multichainProviders } = useContext(MultichainContext);
   const [availability, setAvailability] = useState<ContractAvailability>({});
   const { chainId: currentChainId } = useWeb3React();
