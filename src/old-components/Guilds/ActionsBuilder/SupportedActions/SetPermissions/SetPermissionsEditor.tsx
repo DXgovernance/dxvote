b3868affa21ff2b17090e60235394e60aa4a133d
Dino <crescimbenidino@gmail.com>
2022-05-12 15:23:41 -0300

    feat: refactored asset transfer and function call permissions in its own components

--- src/old-components/Guilds/ActionsBuilder/SupportedActions/SetPermissions/SetPermissionsEditor.tsx
+++ src/old-components/Guilds/ActionsBuilder/SupportedActions/SetPermissions/SetPermissionsEditor.tsx
@@ -1,51 +1,14 @@
 import { useMemo, useState } from 'react';
-import styled from 'styled-components';
 import { useWeb3React } from '@web3-react/core';
 import { BigNumber, utils } from 'ethers';
 import { MAINNET_ID } from 'utils';
-import { resolveUri } from 'utils/url';
 import { ActionEditorProps } from '..';
 import { useERC20Info } from 'hooks/Guilds/ether-swr/erc20/useERC20Info';
 import useENSAvatar from 'hooks/Guilds/ether-swr/ens/useENSAvatar';
 import { useTokenList } from 'hooks/Guilds/tokens/useTokenList';
-import { Box } from 'Components/Primitives/Layout';
-import Input from 'old-components/Guilds/common/Form/Input';
-import Avatar from 'old-components/Guilds/Avatar';
-import TokenAmountInput from 'old-components/Guilds/common/Form/TokenAmountInput';
 import TokenPicker from 'old-components/Guilds/TokenPicker';
-import { Button } from 'old-components/Guilds/common/Button';
-import { FiChevronDown, FiX } from 'react-icons/fi';
-
-const Control = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  margin: 0.75rem 0;
-  width: 100%;
-`;
-
-const ControlLabel = styled(Box)`
-  margin-bottom: 0.75rem;
-`;
-
-const ControlRow = styled(Box)`
-  display: flex;
-  align-items: stretch;
-  height: 100%;
-`;
-
-const ClickableIcon = styled(Box)`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const Spacer = styled(Box)`
-  margin-right: 1rem;
-`;
-
-const OneLineButton = styled(Button)`
-  white-space: nowrap;
-`;
+import AssetTransfer from './AssetTransfer';
+import FunctionCall from './FunctionCall';
 
 interface PermissionState {
   source: string;
@@ -129,108 +92,23 @@ const Permissions: React.FC<ActionEditorProps> = ({
 
   return (
     <div>
-      Asset transfer permissions
-      <Control>
-        <ControlLabel>Recipient</ControlLabel>
-        <ControlRow>
-          <Input
-            value={''}
-            icon={
-              <div>
-                {validations.destination && (
-                  <Avatar
-                    src={destinationAvatarUrl}
-                    defaultSeed={parsedData.destination}
-                    size={24}
-                  />
-                )}
-              </div>
-            }
-            iconRight={
-              parsedData?.destination ? (
-                <ClickableIcon onClick={() => setTransferAddress('')}>
-                  <FiX size={18} />
-                </ClickableIcon>
-              ) : null
-            }
-            placeholder="Ethereum address"
-            onChange={e => setTransferAddress(e.target.value)}
-          />
-        </ControlRow>
-      </Control>
-      <Control>
-        <ControlLabel>Amount</ControlLabel>
-        <ControlRow>
-          <TokenAmountInput
-            decimals={tokenInfo?.decimals}
-            value={parsedData?.amount}
-            onChange={setAmount}
-          />
-          <Spacer />
-          <OneLineButton>Max Value</OneLineButton>
-        </ControlRow>
-      </Control>
-      <Control>
-        <ControlLabel>Asset</ControlLabel>
-        <ControlRow onClick={() => setIsTokenPickerOpen(true)}>
-          <Input
-            value={tokenInfo?.symbol || ''}
-            placeholder="Token"
-            icon={
-              <div>
-                {parsedData?.tokenAddress && (
-                  <Avatar
-                    src={resolveUri(token?.logoURI)}
-                    defaultSeed={parsedData?.tokenAddress}
-                    size={18}
-                  />
-                )}
-              </div>
-            }
-            iconRight={<FiChevronDown size={24} />}
-            readOnly
-          />
-        </ControlRow>
-      </Control>
-      Function calls permissions
-      <Control>
-        <ControlLabel>Recipient</ControlLabel>
-        <ControlRow>
-          <Input
-            value={''}
-            icon={
-              <div>
-                {validations.destination && (
-                  <Avatar
-                    src={destinationAvatarUrl}
-                    defaultSeed={parsedData.destination}
-                    size={24}
-                  />
-                )}
-              </div>
-            }
-            iconRight={
-              parsedData?.destination ? (
-                <ClickableIcon onClick={() => setTransferAddress('')}>
-                  <FiX size={18} />
-                </ClickableIcon>
-              ) : null
-            }
-            placeholder="Ethereum address"
-            onChange={e => setTransferAddress(e.target.value)}
-          />
-        </ControlRow>
-      </Control>
-      <Control>
-        <ControlLabel>Function signature</ControlLabel>
-        <ControlRow>
-          <Input
-            value={''}
-            placeholder="Function signature"
-            onChange={() => console.log('change function signature input')}
-          />
-        </ControlRow>
-      </Control>
+      Toggle type
+      <AssetTransfer
+        validations={validations}
+        destinationAvatarUrl={destinationAvatarUrl}
+        parsedData={parsedData}
+        setTransferAddress={setTransferAddress}
+        tokenInfo={tokenInfo}
+        setAmount={setAmount}
+        setIsTokenPickerOpen={setIsTokenPickerOpen}
+        token={token}
+      />
+      <FunctionCall
+        validations={validations}
+        destinationAvatarUrl={destinationAvatarUrl}
+        parsedData={parsedData}
+        setTransferAddress={setTransferAddress}
+      />
       <TokenPicker
         walletAddress={parsedData?.source || ''}
         isOpen={isTokenPickerOpen}
