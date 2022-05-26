93a8682f3da92fe955b82128f83a5a1c96ad1f31
Milton <milton.tulli@gmail.com>
2022-05-26 17:31:24 -0300

    remove coment

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@ module.exports = {
   'src/**/*.{ts,tsx,json}': [
     'yarn run format',
     'yarn run lint --fix',
-    // TODO: consider adding tests later if neccesary
     'yarn run test --bail --findRelatedTests',
   ],
 };
