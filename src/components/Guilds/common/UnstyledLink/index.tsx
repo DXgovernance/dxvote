776c4ddb5dd9a51cd97b5aac00180f253bdf0ce9
AugustoL <augustolemble@pm.me>
2022-06-01 10:23:31 -0300

    refactor(src): remove guilds fiels cause it wont be used in the app

--- src/components/Guilds/common/UnstyledLink/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const UnstyledLink = styled(Link)`
-  text-decoration: inherit;
-  color: inherit;
-`;
-
-export default UnstyledLink;
