7cb82b8e85c280101cc6232717cb533e6b60444a
AugustoL <augustolemble@pm.me>
2022-11-24 09:01:14 -0300

    fix(ipfshashes): now save decoded ipfsHash on cache and decode when loading, supports v1 format

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -2,26 +2,12 @@ import contentHash from 'content-hash';
 
 export const descriptionHashToIPFSHash = function (descriptionHash) {
   try {
-    if (contentHash.getCodec(descriptionHash) === 'ipfs-ns')
+    if (contentHash.getCodec(descriptionHash) === 'ipfs-ns') {
       return contentHash.decode(descriptionHash);
-    else if (
-      descriptionHash.length > 1 &&
-      descriptionHash.substring(0, 2) !== 'Qm'
-    )
+    } else {
       return descriptionHash;
-    else return '';
+    }
   } catch (error) {
-    return '';
-  }
-};
-
-export const ipfsHashToDescriptionHash = function (ipfsHash) {
-  try {
-    if (ipfsHash.length > 1 && ipfsHash.substring(0, 2) === 'Qm')
-      return contentHash.fromIpfs(ipfsHash);
-    else if (contentHash.getCodec(ipfsHash) === 'ipfs-ns') return ipfsHash;
-    else return '';
-  } catch (error) {
-    return '';
+    return descriptionHash;
   }
 };
